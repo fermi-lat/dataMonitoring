26c8509823a4a648ffe5e168c8ef3dfc43eb14ca
bregeon <>
2007-03-15 17:14:49 +0000

    added cal_tower_count

--- FastMon/python/pCALcontributionIteratorBase.py
+++ FastMon/python/pCALcontributionIteratorBase.py
@@ -3,6 +3,8 @@ import LDF
 
 from copy import copy
 
+## @brief Base Class for the CAL contribution iterator
+
 class pCALcontributionIteratorBase(LDF.CALcontributionIterator):
 
     def __init__(self, event, contribution, treeMaker):
@@ -15,3 +17,12 @@ class pCALcontributionIteratorBase(LDF.CALcontributionIterator):
     
     def cal_log_count(self):
         self.getVariable("cal_log_count")[self.TemId] = copy(self.contribution().numLogAccepts())
+
+    ## @brief Fill cal_tower_count tree branch
+    ## cal_tower_count is the number of calorimeters with at least one log hit
+    ## @param self
+    #  The class instance.
+    def cal_tower_count(self):
+        if self.contribution().numLogAccepts() > 0:
+	    self.getVariable("cal_tower_count")[0] += 1
+
