9e120364917726d89129f006972c0eb515daa837
bregeon <>
2007-03-21 14:30:53 +0000

    static variables are now declared in baseConfig.xml and comes for free in the VariablesDictionnary

--- FastMon/python/pRootTreeMaker.py
+++ FastMon/python/pRootTreeMaker.py
@@ -4,6 +4,7 @@ import logging
 
 from ROOT                        import TFile, TTree, TH1F
 from pRootTreeProcessor          import pRootTreeProcessor
+from pXmlParser                       import pXmlParser
 
 
 class pRootTreeMaker:
@@ -13,32 +14,22 @@ class pRootTreeMaker:
         self.__RootTree  = TTree('IsocDataTree', 'IsocDataTree')
         self.__XmlParser = xmlParser
         self.__Processor = pRootTreeProcessor(self.__RootTree,\
-                                              self.__XmlParser)
-        self.DefaultVariablesDictionary = None
-        self.VariablesDictionary = None        
-        self.__createTree()
+                                              self.__XmlParser)					      	
+        self.VariablesDictionary = {}        
+        self.__createBranches()
         logging.info('Created File %s with Tree %s\n' % (outputFilePath, self.__RootTree.GetName()) )
 
-    def __createTree(self):
-        self.VariablesDictionary = {}
-        self.__createDefaultBranches()
+    def __createBranches(self):
         for variable in self.__XmlParser.EnabledVariablesDict.values():
             self.VariablesDictionary[variable.getName()] = variable.Array
             self.__createTreeBranch(variable)
+        
 
     def __createTreeBranch(self, variable):
         self.__RootTree.Branch(variable.getName(),\
                                self.VariablesDictionary[variable.getName()],\
                                variable.LeafList)
 
-    def __createDefaultBranches(self):
-        self.DefaultVariablesDictionary = {}
-        variable_name = 'event_timestamp'
-        variable = numpy.zeros(1, 'double')
-        self.DefaultVariablesDictionary[variable_name] = variable
-        self.__RootTree.Branch(variable_name, variable,\
-                               '%s[1]/D' % variable_name)
-        
     def fillTree(self):
         self.__RootTree.Fill()
 
@@ -47,10 +38,8 @@ class pRootTreeMaker:
     #  The class instance.
 
     def resetVariables(self):
-
         for variable in self.__XmlParser.EnabledVariablesDict.values():
-            variable.reset()
-	    
+            variable.reset()	    
 	    
 
     def close(self):
