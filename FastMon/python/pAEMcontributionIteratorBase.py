e88b993edf3cbd3ef9ca5ed2f2c640a573735e3f
bregeon <>
2007-03-22 18:34:36 +0000

    added some feature to play with the ACD

--- FastMon/python/pAEMcontributionIteratorBase.py
+++ FastMon/python/pAEMcontributionIteratorBase.py
@@ -15,5 +15,14 @@ class pAEMcontributionIteratorBase(LDF.AEMcontributionIterator):
         return self.TreeMaker.VariablesDictionary[name]
 
     def acd_tile_count__pha__(self, cable, channel, pha):
+	#print cable, channel, pha.ADCrange(), pha.ADCvalue() 
 	self.getVariable('acd_tile_count')[0] += 1
    
+    def acd_tile_hitmap__header__(self, cable, header):
+	#print cable, hex(header.hitMap())
+	self.getVariable('acd_tile_hitmap')[0] = header.hitMap()
+
+    def acd_cable_tile_count__pha__(self, cable, channel, pha):
+	#print cable, channel, pha.ADCrange(), pha.ADCvalue() 
+	self.getVariable('acd_cable_tile_count')[cable] += 1
+
