a387d723e983801ab87e818ba25cbab3c35c438b
bregeon <>
2007-03-29 15:52:31 +0000

    some gem variables in ms

--- FastMon/python/pGEMcontributionBase.py
+++ FastMon/python/pGEMcontributionBase.py
@@ -1,8 +1,10 @@
 
+
 import logging
 import LDF
 
-from copy import copy
+from copy     import copy
+from pGlobals import *
 
 class pGEMcontributionBase:
     
@@ -17,65 +19,79 @@ class pGEMcontributionBase:
         return getattr(self.__Contribution, attr)
 
     def gem_timebase(self):
-        self.getVariable("gem_timebase")[0] = copy(self.triggerTime())
+        self.getVariable('gem_timebase')[0] = copy(self.triggerTime())
 
 # Discarded and Dead zone
     def gem_discarded(self):
-        self.getVariable("gem_discarded")[0] = copy(self.discarded())
+        self.getVariable('gem_discarded')[0] = copy(self.discarded())
 	
     def gem_dead_zone(self):
-        self.getVariable("gem_dead_zone")[0] = copy(self.deadZone())
+        self.getVariable('gem_dead_zone')[0] = copy(self.deadZone())
 	
     def gem_live_time(self):
-        self.getVariable("gem_live_time")[0] = copy(self.liveTime())
+        self.getVariable('gem_live_time')[0] = copy(self.liveTime())
 	
 # Trigger Vector
     def gem_condition_summary(self):
-        self.getVariable("gem_condition_summary")[0] = copy(self.conditionSummary())
+        self.getVariable('gem_condition_summary')[0] = copy(self.conditionSummary())
 
     def gem_tkr_vector(self):
-        self.getVariable("gem_tkr_vector")[0] = copy(self.tkrVector())
+        self.getVariable('gem_tkr_vector')[0] = copy(self.tkrVector())
 
     def gem_cal_le_vector(self):
-        self.getVariable("gem_cal_le_vector")[0] = copy(self.calLEvector())
+        self.getVariable('gem_cal_le_vector')[0] = copy(self.calLEvector())
 
     def gem_cal_he_vector(self):
-        self.getVariable("gem_cal_he_vector")[0] = copy(self.calHEvector())
+        self.getVariable('gem_cal_he_vector')[0] = copy(self.calHEvector())
 
 # Arrival Time
     def gem_cond_arr_time_tkr(self):
-        self.getVariable("gem_cond_arr_time_tkr")[0] = copy(self.condArrTime().tkr())
+        self.getVariable('gem_cond_arr_time_tkr')[0] = copy(self.condArrTime().tkr())
 
     def gem_cond_arr_time_cal_le(self):
-        self.getVariable("gem_cond_arr_time_cal_le")[0] = copy(self.condArrTime().calLE())
+        self.getVariable('gem_cond_arr_time_cal_le')[0] = copy(self.condArrTime().calLE())
 
     def gem_cond_arr_time_cal_he(self):
-        self.getVariable("gem_cond_arr_time_cal_he")[0] = copy(self.condArrTime().calHE())
+        self.getVariable('gem_cond_arr_time_cal_he')[0] = copy(self.condArrTime().calHE())
 
     def gem_cond_arr_time_roi(self):
-        self.getVariable("gem_cond_arr_time_roi")[0] = copy(self.condArrTime().roi())
+        self.getVariable('gem_cond_arr_time_roi')[0] = copy(self.condArrTime().roi())
 
     def gem_cond_arr_time_cno(self):
-        self.getVariable("gem_cond_arr_time_cno")[0] = copy(self.condArrTime().cno())
+        self.getVariable('gem_cond_arr_time_cno')[0] = copy(self.condArrTime().cno())
 
 # Time Info
     def gem_trigger_time(self):
-        self.getVariable("gem_trigger_time")[0] = copy(self.triggerTime())
-	#print "GEM Trigger Time ", self.triggerTime()
+        self.getVariable('gem_trigger_time')[0] = copy(self.triggerTime())
+	#print 'GEM Trigger Time ', self.triggerTime()
 
     def gem_one_pps_time_time_base(self):
-        self.getVariable("gem_one_pps_time_time_base")[0] = copy(self.onePPStime().timebase())
-	#print "GEM Time Base ", self.onePPStime().timebase()
+        self.getVariable('gem_one_pps_time_time_base')[0] = copy(self.onePPStime().timebase())
+	#print 'GEM Time Base ', self.onePPStime().timebase()
 
     def gem_one_pps_time_seconds(self):
-        self.getVariable("gem_one_pps_time_seconds")[0] = copy(self.onePPStime().seconds())
-	#print "GEM Time Seconds ",  self.onePPStime().seconds()
+        self.getVariable('gem_one_pps_time_seconds')[0] = copy(self.onePPStime().seconds())
+	#print 'GEM Time Seconds ',  self.onePPStime().seconds()
+
+    ## @brief Fill the gem_delta_event_time variable.
+    #
+    #  The variable is converted from clock tics to ms.
+    ## @param self
+    #  The class instance.
 
     def gem_delta_event_time(self):
-        self.getVariable("gem_delta_event_time")[0] = copy(self.deltaEventTime())
+        value = self.deltaEventTime()*CLOCK_TIC_MILLISECONDS
+        self.getVariable('gem_delta_event_time')[0] = copy(value)
 
+    ## @brief Fill the gem_delta_window_open_time variable.
+    #
+    #  The variable is converted from clock tics to ms.
+    ## @param self
+    #  The class instance.
+    
     def gem_delta_window_open_time(self):
-        self.getVariable("gem_delta_event_time")[0] = copy(self.deltaWindowOpenTime())
+        value = self.deltaWindowOpenTime()*CLOCK_TIC_MILLISECONDS
+        self.getVariable('gem_delta_window_open_time')[0] = copy(value)
 
 	
 	
