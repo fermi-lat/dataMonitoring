8fd0e9812d72d35896bb337a6fa51caaa2659bd8
lbaldini <>
2007-06-15 13:43:30 +0000

    Initial commit.

---
+++ Common/python/alg__x_min_bin.py
@@ -0,0 +1,40 @@
+
+import ROOT
+
+from pAlarmBaseAlgorithm import pAlarmBaseAlgorithm
+
+
+## @brief Return position of the center of the first populated bin of
+#  the histogram on the x axis.
+#
+#  Valid parameters: None.
+#
+## @param plot
+#  The ROOT object.
+## @param paramsDict
+#  The (optional) dictionary of parameters.
+
+class alg__x_min_bin(pAlarmBaseAlgorithm):
+
+    SUPPORTED_TYPES      = ['TH1F']
+    SUPPORTED_PARAMETERS = []
+
+    def __init__(self, limits, object, paramsDict = {}):
+        pAlarmBaseAlgorithm.__init__(self, limits, object, paramsDict)
+
+    def run(self):
+        for bin in range(self.RootObject.GetXaxis().GetFirst(),\
+                         self.RootObject.GetXaxis().GetLast()+1):
+            if self.RootObject.GetBinContent(bin) > 0:
+                self.Output.setValue(self.RootObject.GetBinCenter(bin))
+                return None
+
+
+if __name__ == '__main__':
+    from pAlarmLimits import pAlarmLimits
+    limits = pAlarmLimits(-4, 4, -5, 5)
+    histogram = ROOT.TH1F('h', 'h', 100, -5, 5)
+    histogram.FillRandom('gaus', 1000)
+    algorithm = alg__x_min_bin(limits, histogram)
+    algorithm.apply()
+    print algorithm.Output
