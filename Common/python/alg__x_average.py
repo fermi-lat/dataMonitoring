4f76515cf62b9bca13dc7d256b9748d90762c4f3
lbaldini <>
2007-06-15 13:32:41 +0000

    Documentation (taken from the old pAlarmAlgorithms, to be revised) added.

--- Common/python/alg__x_average.py
+++ Common/python/alg__x_average.py
@@ -4,6 +4,21 @@ import ROOT
 from pAlarmBaseAlgorithm import pAlarmBaseAlgorithm
 
 
+## @brief Return the average of the histogram on the x axis.
+#
+#  The average value is retrieved through the ROOT TH1::GetMean() method.
+#  In case the <tt>min</tt> or <tt>max</tt> parameters are specified,
+#  the range is properly set, first. At the end the range is reset anyway.
+#
+#  Valid parameters:
+#  @li <tt>min</tt>: the minimum x value for the average calculation.
+#  @li <tt>max</tt>: the maximum x value for the average calculation.
+#
+## @param plot
+#  The ROOT object.
+## @param paramsDict
+#  The (optional) dictionary of parameters.
+
 class alg__x_average(pAlarmBaseAlgorithm):
 
     SUPPORTED_TYPES      = ['TH1F']
