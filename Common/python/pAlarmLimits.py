ab13a9beea38568cc6642f761c0472af7d1a7edc
lbaldini <>
2007-06-18 09:21:05 +0000

    Logging improved.

--- Common/python/pAlarmLimits.py
+++ Common/python/pAlarmLimits.py
@@ -13,13 +13,17 @@ class pAlarmLimits:
 
     def __validate(self):
         if self.WarningMin > self.WarningMax:
-            logging.error('Warning min higher than warning max.')
+            logging.error('Warning min (%s) higher than warning max (%s).'  %\
+                          (self.WarningMin, self.WarningMax))
         if self.ErrorMin > self.ErrorMax:
-            logging.error('Error min higher than error max.')
+            logging.error('Error min (%s) higher than error max (%s).'      %\
+                          (self.ErrorMin, self.ErrorMax))
         if self.WarningMin < self.ErrorMin:
-            logging.error('Warning min lower than error min.')
+            logging.error('Warning min (%s) lower than error min (%s).'     %\
+                          (self.WarningMin, self.ErrorMin))
         if self.WarningMax > self.ErrorMax:
-            logging.error('Warning max is higher than error max.')
+            logging.error('Warning max (%s) is higher than error max (%s).' %\
+                          (self.WarningMax, self.ErrorMax))
 
     def getTextSummary(self):
         return '[%s/%s; %s/%s]' % (self.ErrorMin, self.WarningMin,
