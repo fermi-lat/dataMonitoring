3203094fb64280d6bbfb51e6cbc6ad6c1c0434e7
lbaldini <>
2007-09-21 09:13:28 +0000

    new alarm algorithm added

---
+++ Common/python/alg__gauss_norm.py
@@ -0,0 +1,39 @@
+from pSafeROOT import ROOT
+
+from pAlarmBaseAlgorithm import pAlarmBaseAlgorithm
+
+
+## @brief Make sure the norm of a gaussian fit is within limits.
+#
+#  @todo Implement support for fitting in a subrange (min and max not
+#  used, at the moment).
+#
+#  Valid parameters:
+#  @li <tt>min</tt>: the minimum x value for the fit.
+#  @li <tt>max</tt>: the maximum x value for the fit.
+
+class alg__gauss_norm(pAlarmBaseAlgorithm):
+
+    SUPPORTED_TYPES      = ['TH1F']
+    SUPPORTED_PARAMETERS = ['min', 'max']
+
+    def __init__(self, limits, object, paramsDict = {}):
+        pAlarmBaseAlgorithm.__init__(self, limits, object, paramsDict)
+
+    def run(self):
+        self.adjustXRange()
+        gaussian = ROOT.TF1('g', 'gaus')
+        self.RootObject.Fit(gaussian, 'QN')
+        self.Output.setValue(gaussian.GetParameter(0))
+        self.resetXRange()
+
+
+if __name__ == '__main__':
+    from pAlarmLimits import pAlarmLimits
+    limits = pAlarmLimits(1, 2, 0, 3)
+    histogram = ROOT.TH1F('h', 'h', 100, -5, 5)
+    histogram.FillRandom('gaus', 1000)
+    dict = {}
+    algorithm = alg__gauss_norm(limits, histogram, dict)
+    algorithm.apply()
+    print algorithm.Output
