c809ccf93e01e8e04fe9482eb44c5661b7d3c233
kocian <>
2007-10-03 23:03:27 +0000

    updated to handle arrays

--- Common/python/alg__values.py
+++ Common/python/alg__values.py
@@ -19,15 +19,16 @@ from pAlarmBaseAlgorithm import pAlarmBaseAlgorithm
 class alg__values(pAlarmBaseAlgorithm):
 
     SUPPORTED_TYPES      = ['TBranch']
-    SUPPORTED_PARAMETERS = []
+    SUPPORTED_PARAMETERS = ['Exclude','Only']
 
     def __init__(self, limits, object, paramsDict = {}):
         pAlarmBaseAlgorithm.__init__(self, limits, object, paramsDict)
         self.RootTree = self.RootObject.GetTree()
+        self.RootLeaf = self.RootTree.GetLeaf(self.RootObject.GetName())
 
-    def getBranchContent(self, entry):
+    def getBranchContent(self, entry,index):
         self.RootObject.GetEntry(entry)
-        return eval('self.RootTree.%s' % self.RootObject.GetName())
+        return self.RootLeaf.GetValue(index)
 
     def run(self):
         self.Output.setDictValue('num_warning_points', 0)
@@ -35,25 +36,54 @@ class alg__values(pAlarmBaseAlgorithm):
         self.Output.setDictValue('warning_points', [])
         self.Output.setDictValue('error_points'  , [])
         for entry in range(self.RootObject.GetEntries()):
-            value = self.getBranchContent(entry)
-            if value < self.Limits.ErrorMin or value > self.Limits.ErrorMax:
-                point = (entry, value)
-                self.Output.incrementDictValue('num_error_points')
-                self.Output.appendDictValue('error_points', point)
-            elif value < self.Limits.WarningMin or value > self.Limits.WarningMax:
-                point = (entry, value)
-                self.Output.incrementDictValue('num_warning_points')
-                self.Output.appendDictValue('warning_points', point)
-            if self.Output.getDictValue('num_error_points'):
-                self.Output.setValue(self.Output.getDictValue('error_points')[0][1])
-            elif self.Output.getDictValue('num_warning_points'):
-                self.Output.setValue(self.Output.getDictValue('warning_points')[0][1])
-            else:
-                entry = self.RootObject.GetEntries()/2
-                self.Output.setValue(self.RootObject.GetBranchContent(entry))
-
-
+            for index in range (self.RootLeaf.GetLen()):
+                indices=self.indexlist(index)
+                if(indices!=[]):
+                    if "Only" in self.ParamsDict:
+                        if not indices in self.ParamsDict["Only"]:
+                            continue
+                    if "Exclude" in self.ParamsDict:
+                        if indices in self.ParamsDict["Exclude"]:
+                            continue
+                value = self.getBranchContent(entry,index)
+                if value < self.Limits.ErrorMin or value > self.Limits.ErrorMax:
+                    if indices==[]:
+                        point = (entry, value)
+                    else:
+                        point = (entry,indices,value)
+                    self.Output.incrementDictValue('num_error_points')
+                    self.Output.appendDictValue('error_points', point)
+                elif value < self.Limits.WarningMin or value > self.Limits.WarningMax:
+                    if indices==[]:
+                        point = (entry, value)
+                    else:
+                        point = (entry,indices,value)
+                    self.Output.incrementDictValue('num_warning_points')
+                    self.Output.appendDictValue('warning_points', point)
+                if self.Output.getDictValue('num_error_points'):
+                    self.Output.setValue(self.Output.getDictValue('error_points')[0][-1])
+                elif self.Output.getDictValue('num_warning_points'):
+                    self.Output.setValue(self.Output.getDictValue('warning_points')[0][-1])
+                else:
+                    self.Output.setValue(value)
                 
+    def indexlist(self,index):
+        name=self.RootLeaf.GetTitle()
+        if name.find('[')==-1:
+            return [];
+        name=name[name.find('['):-1]
+        dims=name.split('][')
+        retlist=[]
+        ind=index
+        for i in range(len(dims)):
+            fact=1
+            for j in range(i+1,len(dims)):
+                fact*=int(dims[j])
+            retlist.append(ind/fact)
+            ind-=ind/fact*fact
+        return retlist
+        
+        
 
 if __name__ == '__main__':
     pass
