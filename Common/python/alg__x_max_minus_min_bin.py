5b3020267eb758a3c8c4e745f66527244ee4f87c
lbaldini <>
2008-01-31 15:55:21 +0000

    Algorithm updated

--- Common/python/alg__x_max_minus_min_bin.py
+++ Common/python/alg__x_max_minus_min_bin.py
@@ -4,35 +4,34 @@ from pSafeROOT import ROOT
 from pAlarmBaseAlgorithm import pAlarmBaseAlgorithm
 
 
+
 ## @brief Return position of the center of the last populated bin of
 #  the histogram on the x axis minus the center of the first populated bin of
-#  the histogram on the x .
+#  the histogram on the x.
 #
-#  Valid parameters: None.
+#  <b>Output value</b>:
 #
-## @param plot
-#  The ROOT object.
-## @param paramsDict
-#  The (optional) dictionary of parameters.
+#  Distance between the first and last populated bins.
+
+
 
 class alg__x_max_minus_min_bin(pAlarmBaseAlgorithm):
 
     SUPPORTED_TYPES      = ['TH1F']
     SUPPORTED_PARAMETERS = []
-
-    def __init__(self, limits, object, paramsDict = {}):
-        pAlarmBaseAlgorithm.__init__(self, limits, object, paramsDict)
+    OUTPUT_DICTIONARY    = {}
+    OUTPUT_LABEL         = 'Distance between first and last populated bin'
 
     def run(self):
         lastBin  = -1
         firstBin = -1
         for bin in range(self.RootObject.GetXaxis().GetLast(),\
-                         self.RootObject.GetXaxis().GetFirst()-1, -1):
+                         self.RootObject.GetXaxis().GetFirst() - 1, - 1):
             if self.RootObject.GetBinContent(bin) > 0:
                 lastBin = bin
                 break
         for bin in range(self.RootObject.GetXaxis().GetFirst(),\
-                         self.RootObject.GetXaxis().GetLast()+1):
+                         self.RootObject.GetXaxis().GetLast() + 1):
             if self.RootObject.GetBinContent(bin) > 0:
                 firstBin = bin
                 break
@@ -47,8 +46,12 @@ class alg__x_max_minus_min_bin(pAlarmBaseAlgorithm):
 if __name__ == '__main__':
     from pAlarmLimits import pAlarmLimits
     limits = pAlarmLimits(-4, 4, -5, 5)
+    canvas = ROOT.TCanvas('Test canvas', 'Test canvas', 400, 400)
+
     histogram = ROOT.TH1F('h', 'h', 100, -5, 5)
     histogram.FillRandom('gaus', 1000)
-    algorithm = alg__x_max_minus_min_bin(limits, histogram)
+    histogram.Draw()
+    canvas.Update()
+    algorithm = alg__x_max_minus_min_bin(limits, histogram, {})
     algorithm.apply()
     print algorithm.Output
