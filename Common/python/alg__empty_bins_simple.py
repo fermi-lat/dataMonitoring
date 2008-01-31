9f1fe72cf1682c295586eeba2fc332c14d19b7a3
lbaldini <>
2008-01-31 11:28:40 +0000

    Test function added, minor formatting changes, output label added.

--- Common/python/alg__empty_bins_simple.py
+++ Common/python/alg__empty_bins_simple.py
@@ -1,6 +1,5 @@
 
 from pSafeROOT import ROOT
-from math      import sqrt
 
 from pAlarmBaseAlgorithm import pAlarmBaseAlgorithm
 
@@ -25,16 +24,24 @@ from pAlarmBaseAlgorithm import pAlarmBaseAlgorithm
 
 class alg__empty_bins_simple(pAlarmBaseAlgorithm):
 
-    SUPPORTED_TYPES      = ['TH2F']
+    SUPPORTED_TYPES      = ['TH1F', 'TH2F']
     SUPPORTED_PARAMETERS = []
     OUTPUT_DICTIONARY    = {'empty_bins': []}
+    OUTPUT_LABEL         = 'Number of empty bins'
 
     ## @brief Basic algorithm evaluation for 1-dimensional histograms.
     ## @param self
     #  The class instance.
 
     def __runTH1(self):
-        print 'Not implemented, yet.'
+        numEmptyBins = 0
+        for i in range(1, self.RootObject.GetNbinsX() + 1):
+            if self.RootObject.GetBinContent(i) == 0:
+                numEmptyBins += 1
+                x = self.RootObject.GetBinCenter(i)
+                binString = 'bin @ %.2f' % x
+                self.Output.appendDictValue('empty_bins', binString)
+        self.Output.setValue(numEmptyBins)
 
     ## @brief Basic algorithm evaluation for 2-dimensional histograms.
     ## @param self
@@ -46,13 +53,52 @@ class alg__empty_bins_simple(pAlarmBaseAlgorithm):
             for j in range(1, self.RootObject.GetNbinsY() + 1):
                 if self.RootObject.GetBinContent(i, j) == 0:
                     numEmptyBins += 1
-                    binString = 'bin = (%d, %d)' % (i - 1, j - 1)
+                    x = self.RootObject.GetXaxis().GetBinCenter(i)
+                    y = self.RootObject.GetYaxis().GetBinCenter(j)
+                    binString = 'bin @ (%.2f, %.2f)' % (x, y)
                     self.Output.appendDictValue('empty_bins', binString)
         self.Output.setValue(numEmptyBins)
 
     ## @brief Overloaded main function.
     ## @param self
     #  The class instance.
-        
+
     def run(self):
-        self.__runTH2()
+        objectType = self.RootObject.__class__.__name__
+        if objectType == 'TH1F':
+            self.__runTH1()
+        elif objectType == 'TH2F':
+            self.__runTH2()
+
+if __name__ == '__main__':
+    from pAlarmLimits import pAlarmLimits
+    canvas = ROOT.TCanvas('Test canvas', 'Test canvas', 600, 300)
+    canvas.Divide(2, 1)
+    limits = pAlarmLimits(-1, 1, -1, 1)
+
+    print
+    print 'Testing on a 1-dimensional histogram...'
+    canvas.cd(1)
+    histogram1d = ROOT.TH1F('h1d', 'h1d', 10, -5, 5)
+    histogram1d.FillRandom('pol0', 1000)
+    histogram1d.SetBinContent(3, 0)
+    histogram1d.Draw()
+    canvas.Update()
+    algorithm1d = alg__empty_bins_simple(limits, histogram1d, {})
+    algorithm1d.apply()
+    print algorithm1d.Output
+
+    print
+    print 'Testing on a 2-dimensional histogram...'
+    canvas.cd(2)
+    histogram2d = ROOT.TH2F('h2d', 'h2d', 10, -5, 5, 10, -5, 5)
+    function2d = ROOT.TF2('f2d', '1 + 0*x + 0*y', -5, 5, -5, 5)
+    function2d.SetParameter(0, 1)
+    histogram2d.FillRandom('f2d', 5000)
+    histogram2d.SetBinContent(2, 2, 0)
+    histogram2d.SetBinContent(4, 4, 0)
+    histogram2d.Draw('colz')
+    canvas.Update()
+    algorithm2d = alg__empty_bins_simple(limits, histogram2d, {})
+    algorithm2d.apply()
+    print algorithm2d.Output
