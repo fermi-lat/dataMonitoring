f7182c48d424ab8850f3b69db49228813223e024
bregeon <>
2008-02-13 16:06:25 +0000

    Update xmin algorithm to take n consecutive bins into account, and add alarms in recon EOR

--- Common/python/alg__x_min_bin.py
+++ Common/python/alg__x_min_bin.py
@@ -5,7 +5,13 @@ from pAlarmBaseAlgorithm import pAlarmBaseAlgorithm
 
 
 ## @brief Position of the center of the first populated bin of
-#  the histogram on the x axis.
+#  the histogram on the x axis, requiring at least "num_adjacent_bins"
+#  consecutive populated bins, in order to remove outliers.
+#  If "num_adjacent_bins" is not specified, the default value is 1
+#  and the algorithm really looks for the first non-empty bin.
+#
+#  If the histogram is empty, then the alarm output is an error and the value
+#  returned is the histogram overflow bin (last bin + 1).
 #
 #  <b>Output value</b>:
 #
@@ -14,16 +20,32 @@ from pAlarmBaseAlgorithm import pAlarmBaseAlgorithm
 class alg__x_min_bin(pAlarmBaseAlgorithm):
 
     SUPPORTED_TYPES      = ['TH1F']
-    SUPPORTED_PARAMETERS = []
+    SUPPORTED_PARAMETERS = ['num_adjacent_bins']
     OUTPUT_DICTIONARY    = {}
     OUTPUT_LABEL         = 'Center of the leftmost populated bin'
 
+    ## @brief Basic algorithm evaluation for 1-dimensional histograms.
+    ## @param self
+    #  The class instance.
+
     def run(self):
-        for bin in range(self.RootObject.GetXaxis().GetFirst(),\
-                         self.RootObject.GetXaxis().GetLast() + 1):
-            if self.RootObject.GetBinContent(bin) > 0:
-                self.Output.setValue(self.RootObject.GetBinCenter(bin))
+        try:
+            numAdjacentBins = self.ParamsDict['num_adjacent_bins']
+        except KeyError:
+            numAdjacentBins = 1
+
+        currentbin = self.RootObject.GetXaxis().GetFirst()        
+        while(currentbin < (self.RootObject.GetXaxis().GetLast() - numAdjacentBins)):   
+            binList = []
+	    for bin in range(currentbin, currentbin+numAdjacentBins):                
+		if self.RootObject.GetBinContent(bin) > 0:
+		    binList.append(bin)
+	    if len(binList) ==  numAdjacentBins:
+	        self.Output.setValue(self.RootObject.GetBinCenter(binList[0]))
                 return None
+            currentbin += numAdjacentBins
+	self.Output.setValue(self.RootObject.GetBinCenter(self.RootObject.GetXaxis().GetLast()+1))
+
 
 
 if __name__ == '__main__':
