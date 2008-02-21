0dfd4ce8c2e4c88288c2747d79b6bad12d9c24a9
lbaldini <>
2008-02-21 08:56:33 +0000

    Some formatting changes in the alarm output.

--- Common/python/alg__x_min_bin_slices.py
+++ Common/python/alg__x_min_bin_slices.py
@@ -2,7 +2,9 @@
 from pSafeROOT import ROOT
 
 from pAlarmBaseAlgorithm import pAlarmBaseAlgorithm
-from alg__x_min_bin import alg__x_min_bin 
+from alg__x_min_bin import alg__x_min_bin
+
+
 
 class alg__x_min_bin_slices(pAlarmBaseAlgorithm):
 
@@ -12,42 +14,45 @@ class alg__x_min_bin_slices(pAlarmBaseAlgorithm):
                             'num_error_slices'  : 0,
                             'warning_slices'    : [],
                             'error_slices'      : []}
-    OUTPUT_LABEL         = ''
+    OUTPUT_LABEL         = 'Bin center of the leftmost bin for the worst slice'
 
     def run(self):
         try:
             numAdjacentBins = self.ParamsDict['num_adjacent_bins']
         except KeyError:
             numAdjacentBins = 1
-
         try:
             sliceWidth = self.ParamsDict['slice_width']
         except KeyError:
             sliceWidth = 1
-	singleSliceParamsDict = {'num_adjacent_bins': numAdjacentBins}
-        currentbin = self.RootObject.GetXaxis().GetFirst()
-        lastbin    = self.RootObject.GetXaxis().GetLast() - sliceWidth
+	sliceParamsDict = {'num_adjacent_bins': numAdjacentBins}
+        currentBin = self.RootObject.GetXaxis().GetFirst()
+        lastBin = self.RootObject.GetXaxis().GetLast() - sliceWidth
+        values = []
 	errorValues = []
 	warningValues = []
-	values = []
-        while(currentbin < lastbin):
-            hslice = self.RootObject.ProjectionY("hslice",currentbin,currentbin+sliceWidth)            
-            singleSliceAlarm = alg__x_min_bin(self.Limits, hslice, singleSliceParamsDict)
-            singleSliceAlarm.apply()
-            sliceCenter = self.RootObject.GetXaxis().GetBinCenter(currentbin + sliceWidth/2)
-	    values.append(singleSliceAlarm.Output.Value)
-	    if singleSliceAlarm.Output.isError():
+        while(currentBin < lastBin):
+            sliceHisto = self.RootObject.ProjectionY("h_single_slice",\
+                                                     currentBin,\
+                                                     currentBin + sliceWidth)
+            sliceAlarm = alg__x_min_bin(self.Limits, sliceHisto,\
+                                        sliceParamsDict)
+            sliceAlarm.apply()
+            sliceCenter = self.RootObject.GetXaxis().GetBinCenter(currentBin +\
+                                                                  sliceWidth/2)
+            sliceLabel = 'slice centered @ %s (min bin center = %s)' %\
+                         (sliceCenter, sliceAlarm.Output.Value)
+	    values.append(sliceAlarm.Output.Value)
+	    if sliceAlarm.Output.isError():
 	        self.Output.incrementDictValue('num_error_slices')
-                self.Output.appendDictValue('error_slices', 'slice centered @ %s (min = %s)' %\
-                                            (sliceCenter, singleSliceAlarm.Output.Value))
-		errorValues.append(singleSliceAlarm.Output.Value)
-	    elif singleSliceAlarm.Output.isWarning():
+                self.Output.appendDictValue('error_slices', sliceLabel)
+		errorValues.append(sliceAlarm.Output.Value)
+	    elif sliceAlarm.Output.isWarning():
 	        self.Output.incrementDictValue('num_warning_slices')
-                self.Output.appendDictValue('warning_slices', 'slice centered @ %s (min = %s)' %\
-                                            (sliceCenter, singleSliceAlarm.Output.Value))
-                warningValues.append(singleSliceAlarm.Output.Value)
-            currentbin += sliceWidth
-            hslice.Delete()
+                self.Output.appendDictValue('warning_slices', sliceLabel)
+                warningValues.append(sliceAlarm.Output.Value)
+            currentBin += sliceWidth
+            sliceHisto.Delete()
 	if self.Output.getDictValue('num_error_slices'):
 	    self.Output.setValue(min(errorValues))
 	elif self.Output.getDictValue('num_warning_slices'):
@@ -66,9 +71,18 @@ if __name__ == '__main__':
     for i in range(3072):
         for j in range(50, 100):
             histogram.SetBinContent(i,j, 100)
-    histogram.SetBinContent(10,10, 50)
-    histogram.SetBinContent(10,11, 20)
-    histogram.SetBinContent(10,12, 23)
+    histogram.SetBinContent(10, 10, 50)
+    histogram.SetBinContent(10, 11, 20)
+    histogram.SetBinContent(10, 12, 23)
+    histogram.SetBinContent(100, 20, 50)
+    histogram.SetBinContent(100, 21, 20)
+    histogram.SetBinContent(100, 22, 23)
+    histogram.SetBinContent(200, 30, 50)
+    histogram.SetBinContent(200, 31, 20)
+    histogram.SetBinContent(200, 32, 23)
+    histogram.SetBinContent(300, 40, 50)
+    histogram.SetBinContent(300, 41, 20)
+    histogram.SetBinContent(300, 42, 23)
     histogram.Draw("colz")
     canvas.Update()
     algorithm = alg__x_min_bin_slices(limits, histogram, {'slice_width': 12})
