696e76e588e1f360ab2451e7f4420bf1c8c93e56
lbaldini <>
2008-03-27 10:12:38 +0000

    All the ACD tiles/ribbons area are defined now. Still need to double check with Alex a couple of numbers.

--- Common/python/pAlarmUtils.py
+++ Common/python/pAlarmUtils.py
@@ -1,38 +1,97 @@
 
+import sys
+import pUtils
 
-## Acd tiles area, from Alex.
-##
-## 0-4, 16-20, 32-36, 48-52 (crown tiles) : 34 x 38cm = 1,292 cm2
-## 5-9, 21-25, 37-41, 53-57 (sides, 2-nd row): 34 x 22 cm = 748 cm2
-## 10-14, 26-30, 42-46, 53-57 (sides, 3-rd row) : 34 x 17 cm = 578 cm2
-## 15, 31, 47, 63 (long tiles): 170 x 17 cm = 2,890 cm2
-## 64-68, 84-88 (top bent tiles) : 40 x 34 cm = 1,360 cm2
-## 69-83 (top 3 middle rows): 34 x 34 cm = 1,156 cm2 
+## @var ACD_CROWN_TILES
+## @brief Information from Alex:
+#  0-4, 16-20, 32-36, 48-52 (crown tiles) : 34 x 38cm = 1,292 cm2
+
+## @var ACD_SIDE_ROW_2_TILES
+## @brief Information from Alex:
+#  5-9, 21-25, 37-41, 53-57 (sides, 2-nd row): 34 x 22 cm = 748 cm2
+## @todo I guess 53-57 is really 58-62. Double check that with Alex.
+
+## @var ACD_SIDE_ROW_3_TILES
+## @brief Information from Alex:
+#  10-14, 26-30, 42-46, 53-57 (sides, 3-rd row) : 34 x 17 cm = 578 cm2
+
+## @var ACD_LONG_TILES
+## @brief Information from Alex:
+#  15, 31, 47, 63 (long tiles): 170 x 17 cm = 2,890 cm2
+
+## @var ACD_TOP_BENT_TILES
+## @brief Information from Alex:
+#  64-68, 84-88 (top bent tiles) : 40 x 34 cm = 1,360 cm2
+
+## @var ACD_TOP_MIDDLE_TILES
+## @brief Information from Alex:
+#  69-83 (top 3 middle rows): 34 x 34 cm = 1,156 cm2
+
+## @var ACD_RIBBONS
+## @brief 96-103: ~600 cm2.
+#  This is an educated guess taken from the rates as they appear in the
+#  data quality monitoring products (after all we need it for normalizing
+#  things, so a reasonable "effective area" should be file).
+## @todo Double check area with Alex.
+
+## @var ACD_UNPHYSICAL_TILES
+## @brief The unphysical tile ids showing up in the data monitoring quantities.
+
+ACD_CROWN_TILES      = pUtils.expandList('0-4, 16-20, 32-36, 48-52')
+ACD_SIDE_ROW_2_TILES = pUtils.expandList('5-9, 21-25, 37-41, 58-62')
+ACD_SIDE_ROW_3_TILES = pUtils.expandList('10-14, 26-30, 42-46, 53-57')
+ACD_LONG_TILES       = pUtils.expandList('15, 31, 47, 63')
+ACD_TOP_BENT_TILES   = pUtils.expandList('64-68, 84-88')
+ACD_TOP_MIDDLE_TILES = pUtils.expandList('69-83')
+ACD_RIBBONS          = pUtils.expandList('96-103')
+ACD_UNPHYSICAL_TILES = pUtils.expandList('89-95, 104-127')
+
+## @brief Return the area of the tile corresponding to a given tile number.
+## @param tileNumber
+## The tile number.
 
 def getAcdTileArea(tileNumber):
-    if tileNumber in [0, 1, 2, 3, 4, 16, 17, 18, 19, 20, 32, 33, 34, 35, 36,
-                      48, 49, 50, 51, 52]:
+    if tileNumber in ACD_CROWN_TILES:
         return 1292.0
-    elif tileNumber in [5, 6, 7, 8, 9, 21, 22, 23, 24, 25, 37, 38, 39, 40, 41,
-                        53, 54, 55, 56, 57]:
+    elif tileNumber in ACD_SIDE_ROW_2_TILES:
         return 748.0
-    elif tileNumber in [10, 11, 12, 13, 14, 26, 27, 28, 29, 30, 42, 43, 44, 45,
-                        46, 53, 54, 55, 56, 57]:
+    elif tileNumber in ACD_SIDE_ROW_3_TILES:
         return 578.0
-    elif tileNumber in [15, 31, 47, 63]:
+    elif tileNumber in ACD_LONG_TILES:
         return 2890.0
-    elif tileNumber in [64, 65, 66, 67, 68, 84, 85, 86, 87, 88]:
+    elif tileNumber in ACD_TOP_BENT_TILES:
         return 1360.0
-    elif tileNumber in range(69, 84):
+    elif tileNumber in ACD_TOP_MIDDLE_TILES:
         return 1156.0
-    else:
+    elif tileNumber in ACD_RIBBONS:
+        return 600.
+    elif tileNumber in ACD_UNPHYSICAL_TILES:
         return 0
+    else:
+        sys.exit('Tile number %d out of range. Abort.' % tileNumber)
+
+## @var ACD_TILES_AREA
+## @brief List of the tiles area, by tile number.
 
 ACD_TILES_AREA = [getAcdTileArea(i) for i in range(128)]
 
 
 if __name__ == '__main__':
+    print 'ACD_CROWN_TILES:'     , ACD_CROWN_TILES
+    print 'ACD_SIDE_ROW_2_TILES:', ACD_SIDE_ROW_2_TILES
+    print 'ACD_SIDE_ROW_3_TILES:', ACD_SIDE_ROW_3_TILES
+    print 'ACD_LONG_TILES:'      , ACD_LONG_TILES
+    print 'ACD_TOP_BENT_TILES:'  , ACD_TOP_BENT_TILES
+    print 'ACD_TOP_MIDDLE_TILES:', ACD_TOP_MIDDLE_TILES
+    print 'ACD_RIBBONS:'         , ACD_RIBBONS
+    print 'ACD_UNPHYSICAL_TILES:', ACD_UNPHYSICAL_TILES
+    print 'Total number of defined tiles: %d' %\
+          len(ACD_CROWN_TILES + ACD_SIDE_ROW_2_TILES + ACD_SIDE_ROW_3_TILES +\
+              ACD_LONG_TILES + ACD_TOP_BENT_TILES + ACD_TOP_MIDDLE_TILES +\
+              ACD_RIBBONS + ACD_UNPHYSICAL_TILES)
+    print
+    print 'Tile\tArea'
     for (tileNumber, area) in  enumerate(ACD_TILES_AREA):
-        print tileNumber, area
+        print '%s\t%s' % (tileNumber, area)
 
 
