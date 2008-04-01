b78155b501b2542f64cc4123392d8c598bdc80ae
lbaldini <>
2008-04-01 10:55:30 +0000

    Chisquare, number of degrees of freedom and reduced chisquare added to the output root file.

--- Common/python/pCalGainAnalyzer.py
+++ Common/python/pCalGainAnalyzer.py
@@ -12,10 +12,10 @@ from pRootFileManager    import pRootFileManager
 from pAlarmBaseAlgorithm import pAlarmBaseAlgorithm
 
 
-BASE_BRANCH_NAME = 'CalXAdcPed_TH1_TowerCalLayerCalColumnFR'
-FIT_RANGE_WIDTH  = 1.5
-CAL_RANGE_DICT   = {0: 'LEX8', 1: 'LEX1', 2: 'HEX8', 3: 'HEX1'}
-
+BASE_BRANCH_NAME  = 'CalXAdcPed_TH1_TowerCalLayerCalColumnFR'
+FIT_RANGE_WIDTH   = 1.5
+CAL_RANGE_DICT    = {0: 'LEX8', 1: 'LEX1', 2: 'HEX8', 3: 'HEX1'}
+HISTOGRAMS_LABELS = ['Mean', 'RMS', 'ChiSquare', 'DOF', 'ReducedChiSquare']
 
 
 class pCalGainAnalyzer(pRootFileManager, pAlarmBaseAlgorithm):
@@ -28,13 +28,17 @@ class pCalGainAnalyzer(pRootFileManager, pAlarmBaseAlgorithm):
         self.__createHistograms()
 
     def __createHistograms(self):
-        self.HistogramsDict = {'Mean': {}, 'RMS': {}}
-        for type in self.HistogramsDict.keys():
-            for (rIndex, rName) in CAL_RANGE_DICT.items():
-                hname = 'CalXAdcPed%s_%s_TH1' % (type, rName)
-                self.HistogramsDict[type][rIndex] =\
-                                                  ROOT.TH1F(hname, hname,\
-                                                            3072, -0.5, 3071.5)
+        self.HistogramsDict = {}
+        for label in HISTOGRAMS_LABELS:
+            self.HistogramsDict[label] = {}
+            for (rangeIndex, rangeName) in CAL_RANGE_DICT.items():
+                histogramName = 'CalXAdcPed%s_%s_TH1' % (label, rangeName)
+                histogram = ROOT.TH1F(histogramName,\
+                                      histogramName, 3072, -0.5, 3071.5)
+                histogram.GetXaxis().SetTitle('Channel number (%s)' %\
+                                              rangeName)
+                histogram.GetYaxis().SetTitle(label)
+                self.HistogramsDict[label][rangeIndex] = histogram
 
     def getHistogramChannel(self, tower, layer, column, face, range = None):
         if range is None:
@@ -42,10 +46,10 @@ class pCalGainAnalyzer(pRootFileManager, pAlarmBaseAlgorithm):
         else:
             return tower*8*12*2*4 + layer*12*2*4 + column*2*4 + face*4 + range
 
-    def fitHistogram(self, tower, layer, column, face, range):
-        name = '%s_%d_%d_%d_%d_%d' %\
-               (BASE_BRANCH_NAME, tower, layer, column, face, range)
-        self.RootObject = self.get(name)
+    def fitChannel(self, tower, layer, column, face, readoutRange):
+        histogramName = '%s_%d_%d_%d_%d_%d' %\
+               (BASE_BRANCH_NAME, tower, layer, column, face, readoutRange)
+        self.RootObject = self.get(histogramName)
         if self.RootObject is None:
             sys.exit('Could not find %s. Abort.' % name)
         mean = self.RootObject.GetMean()
@@ -53,21 +57,35 @@ class pCalGainAnalyzer(pRootFileManager, pAlarmBaseAlgorithm):
         self.ParamsDict['min'] = mean - FIT_RANGE_WIDTH*rms
         self.ParamsDict['max'] = mean + FIT_RANGE_WIDTH*rms
         (norm, mean, rms) = self.getFitParameters(self.Gaussian)
+        chiSquare = self.Gaussian.GetChisquare()
+        dof = self.Gaussian.GetNDF()
+        try:
+            reducedChiSquare = chiSquare/dof
+        except ZeroDivisionError:
+            reducedChiSquare = 0
         channel = self.getHistogramChannel(tower, layer, column, face)
-        self.HistogramsDict['Mean'][range].Fill(channel, mean)
-        self.HistogramsDict['RMS'][range].Fill(channel, rms)
+        self.fillHistogram('Mean', readoutRange, channel, mean)
+        self.fillHistogram('RMS', readoutRange, channel, rms)
+        self.fillHistogram('ChiSquare', readoutRange, channel, chiSquare)
+        self.fillHistogram('DOF', readoutRange, channel, dof)
+        self.fillHistogram('ReducedChiSquare', readoutRange, channel,\
+                           reducedChiSquare)
+        
+    def fillHistogram(self, label, readoutRange, channel, value):
+        self.HistogramsDict[label][readoutRange].Fill(channel, value)
 
     def run(self):
         logger.info('Starting CAL gain analysis...')
         startTime = time.time()
         self.openFile(inputFilePath)
-        for t in range(16):
-            logger.debug('Fitting gains for tower %d...' % t)
-            for l in range(8):
-                for c in range(12):
-                    for f in range(2):
-                        for r in range(4):
-                            self.fitHistogram(t, l, c, f, r)
+        for tower in range(16):
+            logger.debug('Fitting gains for tower %d...' % tower)
+            for layer in range(8):
+                for column in range(12):
+                    for face in range(2):
+                        for readoutRange in range(4):
+                            self.fitChannel(tower, layer, column, face,\
+                                            readoutRange)
         self.closeFile()
         elapsedTime = time.time() - startTime
         logger.info('Done in %.2f s.' % elapsedTime)
@@ -84,15 +102,14 @@ class pCalGainAnalyzer(pRootFileManager, pAlarmBaseAlgorithm):
 
     def drawHistograms(self):
         self.CanvasesDict = {}
-        self.CanvasesDict['Mean'] = ROOT.TCanvas('Mean', 'Mean')
-        self.CanvasesDict['RMS']  = ROOT.TCanvas('RMS', 'RMS')
-        for canvas in self.CanvasesDict.values():
+        for label in HISTOGRAMS_LABELS:
+            canvas = ROOT.TCanvas(label, label)
             canvas.Divide(2, 2)
-        for type in self.HistogramsDict.keys():
+            self.CanvasesDict[label] = canvas
             for i in range(4):
-                self.CanvasesDict[type].cd(i + 1)
-                self.HistogramsDict[type][i].Draw()
-                self.CanvasesDict[type].Update()
+                self.CanvasesDict[label].cd(i + 1)
+                self.HistogramsDict[label][i].Draw()
+                self.CanvasesDict[label].Update()
 
 
 
