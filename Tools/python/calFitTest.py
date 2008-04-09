bd6bd6223e7b9420bc5369a40bcdf607e8127977
lbaldini <>
2008-04-09 13:54:29 +0000

    More test functions added.

--- Tools/python/calFitTest.py
+++ Tools/python/calFitTest.py
@@ -4,47 +4,73 @@ sys.path.append('../../Common/python')
 
 from pCalBaseAnalyzer import *
 
-HYPER_GAUSSIAN.SetParameter(0, 1000)
-HYPER_GAUSSIAN.SetParameter(1, 0.)
-HYPER_GAUSSIAN.SetParameter(2, 1.)
-HYPER_GAUSSIAN.SetParameter(3, 2.)
+
+kFunction = HYPER_GAUSSIAN
+
+kFunction.SetParameter(0, 1)
+kFunction.SetParameter(1, 0.)
+kFunction.SetParameter(2, 1)
+kFunction.SetParameter(3, 2.)
 
 
 def findRMSCorrectionFactors():
     histogram = ROOT.TH1F('testHisto', 'testHisto', 100, -5, 5)
     outputDict = {}
-    for i in range(2, 16):
+    for exponent in range(2, 16):
         histogram.Reset()
-        HYPER_GAUSSIAN.SetParameter(3, i)
-        HYPER_GAUSSIAN.FixParameter(3, i)
+        kFunction.FixParameter(3, exponent)
         histogram.FillRandom('hyper_gaussian', 1000000)
-        histogram.Fit(HYPER_GAUSSIAN, 'Q')
+        histogram.Fit(kFunction, 'Q')
         histogram.Draw()
         ROOT.gPad.Update()
         histogramRms = histogram.GetRMS()
-        fitRms = HYPER_GAUSSIAN.GetParameter(2)
+        fitRms = kFunction.GetParameter(2)
         correctionFactor = histogramRms/fitRms
-        print i, histogramRms, fitRms, correctionFactor
+        print exponent, histogramRms, fitRms, correctionFactor
         outputDict[i] = correctionFactor
         raw_input('Press enter to exit.')
     print outputDict
 
 def testRMSCorrectionFactors():
+    analyzer = pCalBaseAnalyzer(None, None, None)
+    analyzer.FitFunction = kFunction
     histogram = ROOT.TH1F('testHisto', 'testHisto', 100, -5, 5)
-    for i in range(2, 16):
+    for exponent in range(2, 16):
         histogram.Reset()
-        HYPER_GAUSSIAN.SetParameter(3, i)
-        HYPER_GAUSSIAN.FixParameter(3, i)
+        kFunction.FixParameter(3, exponent)
         histogram.FillRandom('hyper_gaussian', 1000000)
-        histogram.Fit(HYPER_GAUSSIAN, 'Q')
+        histogram.Fit(kFunction, 'Q')
         histogram.Draw()
         ROOT.gPad.Update()
         histogramRms = histogram.GetRMS()
-        fitRms = HYPER_GAUSSIAN.GetParameter(2)*HYPER_GAUSSIAN_RMS_DICT[i]
-        print i, histogramRms, fitRms
+        fitRms = kFunction.GetParameter(2)*analyzer.getRmsCorrectionFactor()
+        print exponent, histogramRms, fitRms
         raw_input('Press enter to exit.')
+
+def findNormalization():
+    outputDict = {}
+    for exponent in range(2, 16):
+        kFunction.SetParameter(3, exponent)
+        integral = kFunction.Integral(-5, 5)
+        print exponent, integral
+        outputDict[exponent] = 1./integral
+    print outputDict
+
+def testNormalization():
+    analyzer = pCalBaseAnalyzer(None, None, None)
+    analyzer.FitFunction = kFunction
+    for exponent in range(2, 16):
+        for sigma in [0.5, 1.0, 1.5]:
+            kFunction.SetParameter(3, exponent)
+            kFunction.SetParameter(2, sigma)
+            integral =\
+                 kFunction.Integral(-5, 5)*analyzer.getNormCorrectionFactor()
+            print exponent, sigma, integral
+
 
 
 if __name__ == '__main__':
     #findRMSCorrectionFactors()
-    testRMSCorrectionFactors()
+    #testRMSCorrectionFactors()
+    #findNormalization()
+    testNormalization()
