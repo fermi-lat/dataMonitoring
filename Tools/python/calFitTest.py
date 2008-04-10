b1aa00d42c6555a34fc05dd1f4f160a1bbd56180
lbaldini <>
2008-04-10 12:06:57 +0000

    Quite a few bug fixes and dictionaries for sigma and normalization correction factors replaced with TGraphs.

--- Tools/python/calFitTest.py
+++ Tools/python/calFitTest.py
@@ -2,7 +2,7 @@
 import sys
 sys.path.append('../../Common/python')
 
-from pCalBaseAnalyzer import *
+from pBaseAnalyzer import *
 
 
 kFunction = HYPER_GAUSSIAN
@@ -11,6 +11,8 @@ kFunction.SetParameter(0, 1)
 kFunction.SetParameter(1, 0.)
 kFunction.SetParameter(2, 1)
 kFunction.SetParameter(3, 2.)
+
+kExponentsList = [1.1, 1.2, 1.5, 1.75, 2, 3, 4, 5, 7, 10, 15, 20]
 
 
 def testHyperGaussian():
@@ -23,8 +25,9 @@ def testHyperGaussian():
 
 def findRMSCorrectionFactors():
     histogram = ROOT.TH1F('testHisto', 'testHisto', 100, -5, 5)
-    outputDict = {}
-    for exponent in range(2, 16):
+    fitFunction = ROOT.TF1('fit', '[0] + [1]*(x**[2])')
+    graph = ROOT.TGraph()
+    for (i, exponent) in enumerate(kExponentsList):
         histogram.Reset()
         kFunction.FixParameter(3, exponent)
         histogram.FillRandom('hyper_gaussian', 1000000)
@@ -35,15 +38,23 @@ def findRMSCorrectionFactors():
         fitRms = kFunction.GetParameter(2)
         correctionFactor = histogramRms/fitRms
         print exponent, histogramRms, fitRms, correctionFactor
-        outputDict[i] = correctionFactor
-        raw_input('Press enter to exit.')
-    print outputDict
+        graph.SetPoint(i, exponent, correctionFactor)
+    ROOT.gPad.Clear()
+    graph.Draw('A*')
+    graph.Fit(fitFunction)
+    ROOT.gPad.Update()
+    constant = fitFunction.GetParameter(0)
+    prefactor = fitFunction.GetParameter(1)
+    index = fitFunction.GetParameter(2)
+    formula = '%f + %f*(exponent**(%f))' % (constant, prefactor, index)
+    print formula
+    raw_input()
 
 def testRMSCorrectionFactors():
-    analyzer = pCalBaseAnalyzer(None, None, None)
+    analyzer = pBaseAnalyzer(None, None, None)
     analyzer.FitFunction = kFunction
     histogram = ROOT.TH1F('testHisto', 'testHisto', 100, -5, 5)
-    for exponent in range(2, 16):
+    for exponent in kExponentsList:
         histogram.Reset()
         kFunction.FixParameter(3, exponent)
         histogram.FillRandom('hyper_gaussian', 1000000)
@@ -56,18 +67,27 @@ def testRMSCorrectionFactors():
         raw_input('Press enter to exit.')
 
 def findNormalization():
-    outputDict = {}
-    for exponent in range(2, 16):
+    fitFunction = ROOT.TF1('fit', '[0] + [1]*(x**[2])')
+    graph = ROOT.TGraph()
+    for (i, exponent) in enumerate(kExponentsList):
         kFunction.SetParameter(3, exponent)
         integral = kFunction.Integral(-5, 5)
-        print exponent, integral
-        outputDict[exponent] = 1./integral
-    print outputDict
+        print exponent, 1./integral
+        graph.SetPoint(i, exponent, integral)
+    graph.Draw('A*')
+    graph.Fit(fitFunction)
+    ROOT.gPad.Update()
+    constant = fitFunction.GetParameter(0)
+    prefactor = fitFunction.GetParameter(1)
+    index = fitFunction.GetParameter(2)
+    formula = '%f + %f*(exponent**(%f))' % (constant, prefactor, index)
+    print formula
+    raw_input()
 
 def testNormalization():
-    analyzer = pCalBaseAnalyzer(None, None, None)
+    analyzer = pBaseAnalyzer(None, None, None)
     analyzer.FitFunction = kFunction
-    for exponent in range(2, 16):
+    for exponent in kExponentsList:
         for sigma in [0.5, 1.0, 1.5]:
             kFunction.SetParameter(3, exponent)
             kFunction.SetParameter(2, sigma)
@@ -78,8 +98,8 @@ def testNormalization():
 
 
 if __name__ == '__main__':
-    testHyperGaussian()
+    #testHyperGaussian()
     #findRMSCorrectionFactors()
     #testRMSCorrectionFactors()
     #findNormalization()
-    #testNormalization()
+    testNormalization()
