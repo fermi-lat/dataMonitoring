c0c03f2646e11122578f2254806504ea22f9c737
bregeon <>
2008-06-13 19:37:41 +0000

    Bug fix when calculating the phi angle of the 3D Position vector, now using safer ROOT.TVector3 methods, Longitudes looks nicer. GDQMQ-133

--- FastMon/python/pSCPosition.py
+++ FastMon/python/pSCPosition.py
@@ -295,22 +295,26 @@ class pSCPosition:
         return self.getJulianDate(2000,1,1,12)
 
     ## @brief Returns the Greenwich Meridian Sideral Time for a given Julian Date
+    #  Routine was checked against astro package code, Jun 13th 2008 JB
     ## @param self
     #  The class instance is actually not used in this stand alone function
     ## @param jd
     #  A Julian Date
     def getGMSTime(self, jd):
-	# integer part
+	# integer part - Not Used
 	M=math.modf(jd-0.5)[1] 
 	# fractional part
-	Ora_Un_Dec = math.modf(jd-0.5)[0]*24 
+	Ora_Un_Dec = math.modf(jd-0.5)[0]*24. 
 	
-	jd-=Ora_Un_Dec/24
+	jd-=Ora_Un_Dec/24.
 	
      	T = (jd - self.getJ2000()) / 36525.
      	T1 = (24110.54841 + 8640184.812866 * T + 0.0093103 * T * T)/86400.0
      	
 	Tempo_Siderale_0 = math.modf(T1)[0] * 24.
+        # integer part - Not Used
+	M = math.modf(T1)[1]  
+	
      	Tempo_Siderale_Ora = Tempo_Siderale_0 + Ora_Un_Dec * 1.00273790935
         
 	if Tempo_Siderale_Ora < 0.:
@@ -323,6 +327,7 @@ class pSCPosition:
 
 
     ## @brief Calculate and return the space craft position in Earth coordinates
+    #  Routine was checked against astro package code, Jun 13th 2008 JB
     ## @param self
     #  The class instance.
     def getEarthCoordinate(self):
@@ -330,9 +335,12 @@ class pSCPosition:
  	x = self.Position[0]
 	y = self.Position[1]
 	z = self.Position[2]
-	r = math.sqrt(x*x + y*y + z*z)
-	theta = math.acos(z/r)
-	phi   = math.atan(y/x)
+
+        # use ROOT TVector3 to avoid dumb errors
+	v3    = ROOT.TVector3(x, y, z)
+	r     = v3.Mag()
+	theta = v3.Theta()
+        phi   = v3.Phi()
 	
 	# Latitude
         m_lat = math.pi/2. - theta
@@ -345,10 +353,10 @@ class pSCPosition:
     	if m_lon>math.pi:
 	     m_lon-=2.*math.pi
 
-    	# oblateness correction to obtain geodedic latitude 
+    	# oblateness correction to obtain geodesic latitude 
     	m_lat = math.atan(math.tan(m_lat)) /( (1.-EARTH_FLAT)*(1.-EARTH_FLAT) ) 
 
-    	# this is also such a correction: the number 0.00669454 is the geodetic eccentricity squared?
+    	# this is also such a correction: the number 0.00669454 is the geodesic eccentricity squared?
     	# see http://www.cage.curtin.edu.au/~will/gra64_05.pdf
     	# or http://www.colorado.edu/geography/gcraft/notes/datum/gif/ellipse.gif
     	m_altitude=math.sqrt(x*x+y*y)/math.cos(m_lat)\
