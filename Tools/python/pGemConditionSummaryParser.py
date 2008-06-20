85277db75f3c287c8e8666dfca69bf8dbc31e0cd
lbaldini <>
2008-06-20 21:27:15 +0000

    Impovements in the gem parser.

--- Tools/python/pGemConditionSummaryParser.py
+++ Tools/python/pGemConditionSummaryParser.py
@@ -2,14 +2,26 @@
 from pHtmlWriter import *
 
 
-BITS_LIST = ['ROI', 'TKR', 'CAL_LO', 'CAL_HI', 'CNO', 'PERIODIC']
+BITS_LIST = ['ROI', 'TKR', 'CAL_LO', 'CAL_HI', 'CNO', 'PERIODIC',\
+                 'SOLICITED', 'EXTERNAL']
+FORMATTED_BITS_LIST = ''
+for (bit, label) in enumerate(BITS_LIST):
+    FORMATTED_BITS_LIST += 'bit %d (decimal %d): %s<br/>' %\
+        (bit, 2**bit, label)
 MAIN_TEXT =\
 """
 The following table maps the (decimal) values of the GEMcondition
 summary register to the actual physical content in terms of trigger
-primitives.
-"""
+primitives.<br/>
 
+The GEM condition summary is a 8-bit register mapping the trigger primitives
+in the following order:
+<div class=exampleCode>
+%s
+</div>
+<p>All the combinations of the first 6 bits (which are the most useful) are
+explicitely listed in the table.</p>
+""" % FORMATTED_BITS_LIST
 
 class pGemConditionSummaryParser(pHtmlWriter):
     
@@ -26,7 +38,7 @@ class pGemConditionSummaryParser(pHtmlWriter):
         self.openPage(outputFilePath, 'GEM Condition Summary')
         self.addParagraph(MAIN_TEXT)
         self.openTable(['Condition summary', 'Mapping to primitives'])
-        for i in range(128):
+        for i in range(64):
             self.writeTableLine([i, self.getMapping(i)])
         self.closePage()
 
