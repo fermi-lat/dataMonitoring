424e16a42345a20e6c212552bac3fff658ac16f6
lbaldini <>
2008-06-24 01:22:46 +0000

    Script to wrap the RunVerify.cxx and extract the error code committed.

---
+++ Tools/python/pVerifyErrorCodesParser.py
@@ -0,0 +1,34 @@
+
+from pHtmlWriter import *
+
+
+MAIN_TEXT =\
+"""
+The following table contains some explanations about the Verify error
+codes.</br>
+"""
+
+
+class pVerifyErrorCodesParser(pHtmlWriter):
+    
+    def writeTable(self, inputFilePath, outputFilePath):
+        self.openPage(outputFilePath, 'Verify error codes')
+        self.addParagraph(MAIN_TEXT)
+        self.openTable(['Error code', 'Description'])
+        for line in file(inputFilePath).readlines():
+            try:
+                (pattern, content) = line.split('=')
+                if pattern.strip() == 'errorName':
+                    (errorCode, explanation) = content.split(';')
+                    errorCode = errorCode.strip().replace('"', '')
+                    explanation = explanation.strip().replace('//', '')
+                    self.writeTableLine([errorCode, explanation])
+            except:
+                pass
+        self.closePage()
+
+
+if __name__ == '__main__':
+    inputFilePath = '/data/work/isoc/svac/TestReport/src/RunVerify.cxx'
+    parser = pVerifyErrorCodesParser()
+    parser.writeTable(inputFilePath, 'verify_error_codes.html')
