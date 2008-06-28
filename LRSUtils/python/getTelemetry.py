e969ebafa67c05c77b1a9703372c153091267eb9
lbaldini <>
2008-06-28 21:39:11 +0000

    Added.

---
+++ LRSUtils/python/getTelemetry.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import os
+
+from telemetryTrendingInterface import telemetryTrendingInterface
+
+LRS_DATA_PATH = '/nfs/farm/g/glast/u42/ISOC-flight/FswDumps/reports/nonEvent/'
+DATA_BLOCK_SIZE = {'f1001': 19, 'f1002': 35, 'f1003': 28}
+OUTPUT_DIR_PATH = '/nfs/slac/g/glast/users/glground/lbaldini/saa'
+
+filesList = []
+for fileName in os.listdir(LRS_DATA_PATH):
+    for (pattern, blockSize) in DATA_BLOCK_SIZE.items():
+        if pattern in fileName:
+            filePath = os.path.join(LRS_DATA_PATH, fileName)
+            print filePath
+            print blockSize
+            interface = telemetryTrendingInterface(filePath, OUTPUT_DIR_PATH,\
+                                                   blockSize)
+
