5faf167ee645ef0a72fa81e9be37d40365cf949e
lbaldini <>
2008-06-28 18:59:11 +0000

    *** empty log message ***

--- LRSUtils/python/telemetryTrendingInterface.py
+++ LRSUtils/python/telemetryTrendingInterface.py
@@ -1,38 +1,49 @@
-import urllib
-import sys
-import logging
-logging.basicConfig(level = logging.DEBUG)
 
-DB_BASE_URL = 'http://glast-ground.slac.stanford.edu/TelemetryTrending/'
+import os
+
+from lrsUtils import *
 
 
-class urlReader:
+class telemetryTrendingInterface:
+
+    def __init__(self, inputCsvFilePath, outputDirPath):
+        if not os.path.exists(inputCsvFilePath):
+            sys.exit('Could not find %s. Abort.' % inputCsvFilePath)
+        self.InputCsvFileName = os.path.basename(inputCsvFilePath)
+        self.OutputDirPath = outputDirPath
+        self.FirstTimestamp = lrsUtils.getFirstTimestamp(inputCsvFilePath)
+        self.LastTimestamp = lrsUtils.getLastTimestamp(inputCsvFilePath,\
+                                                           self.DATA_BLOCK_SIZE)
+        self.BeginDate = lrsUtils.utc2string(self.FirstTimestamp)
+        self.EndDate = lrsUtils.utc2string(self.LastTimestamp)
+        logging.info('Data found between %s and %s.' %\
+                         (self.BeginDate, self.EndDate))
+        self.retrieveNavigationInformation()
+        self.retrieveSAAInformation()
     
-    def open(self, url):
-        return urllib.urlopen(url)
-        
-    def get(self, url):
-        logging.debug('Opening url %s' % url)
-        urlObject = self.open(url)
-        logging.debug('Getting data...')
-        data = urlObject.readlines(url)
-        urlObject.close()
-        logging.debug('Done.')
-        return data
+    def retrieveNavigationInformation(self):
+        navFileName = self.InputCvsFileName.replace('.cvs', '_nav.txt')
+        navFilePath = os.path.join(self.OutputDirPath, navFileName)
+        command = 'source /u/gl/glastops/flightops.csh;'
+        command += 'DiagRet.py --nav -b "%s" -e "%s" >> %s' %\
+            (self.BeginDate, self.EndDate, navFilePath)
+        logging.info('About to execute command "%s".' % command)
 
-
-class telemetryTrendingInterface(urlReader):
-
-    def __init__(self, startTime, endTime):
-        self.StartTime = startTime
-        self.EndTime = endTime
-
-    def getData(self, mnemonic):
-        dataUrl = '%sgetData?%s&timeInterval=%d-%d' %\
-            (DB_BASE_URL, mnemonic, self.StartTime, self.EndTime)
-        return self.get(dataUrl)
+    def retrieveSAAInformation(self):
+        saaFileName = self.InputCvsFileName.replace('.cvs', '_saa.txt')
+        saaFilePath = os.path.join(self.OutputDirPath, saaFileName)
+        command = 'source /u/gl/glastops/flightops.csh;'
+        command += 'MnemRet.py --csv %s -b -b "%s" -e "%s" SACFLAGLATINSAA'%\
+            (saaFilePath, self.BeginDate, self.EndDate)
+        logging.info('About to execute command "%s".' % command)
 
 
 if __name__ == '__main__':
-    interface = telemetryTrendingInterface(1214425919600, 1214510945000)
-    print interface.getData('LSPGEOLON')
+    from optparse import OptionParser
+    parser = OptionParser(usage = 'usage: %prog [options]')
+    parser.add_option('-o', '--output-dir', dest = 'o',
+                      default = '.', type = str,
+                      help = 'path to the output dir')
+    (opts, args) = parser.parse_args()
+    interface = telemetryTrendingInterface(args[0], opts.o)
+
