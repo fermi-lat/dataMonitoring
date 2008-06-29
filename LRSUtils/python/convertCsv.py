408cd82db691c4720abc4a4ac28a25f008f9d127
lbaldini <>
2008-06-29 06:07:24 +0000

    Initial import.

---
+++ LRSUtils/python/convertCsv.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+import os
+
+from lrsCalConverter import lrsCalConverter
+from lrsTkrConverter import lrsTkrConverter
+
+LRS_DATA_PATH = '/nfs/farm/g/glast/u42/ISOC-flight/FswDumps/reports/nonEvent'
+FILE_PATTERNS = ['f1001', 'f1002']
+TELEMETRY_DIR_PATH = '/nfs/slac/g/glast/users/glground/lbaldini/saa/telemetry'
+OUTPUT_DIR_PATH = '/nfs/slac/g/glast/users/glground/lbaldini/saa/output'
+
+filesList = []
+for fileName in os.listdir(LRS_DATA_PATH):
+    if 'f1001' in fileName:
+        filePath = os.path.join(LRS_DATA_PATH, fileName)
+        converter = lrsCalConverter(filePath, OUTPUT_DIR_PATH,\
+                                    TELEMETRY_DIR_PATH)
+        converter.convert()
+        converter.close()
+    elif 'f1002' in fileName:
+        filePath = os.path.join(LRS_DATA_PATH, fileName)
+        converter = lrsTkrConverter(filePath, OUTPUT_DIR_PATH,\
+                                    TELEMETRY_DIR_PATH)
+        converter.convert()
+        converter.close()
