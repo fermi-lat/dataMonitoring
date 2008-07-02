d12c2ab183e0e828ec1f99edefb6e737ace35d3e
lbaldini <>
2008-07-02 16:34:17 +0000

    Updated to the new directory structure.

--- LRSUtils/python/getTelemetry.py
+++ LRSUtils/python/getTelemetry.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import os
+import commands
 
 from telemetryTrendingInterface import telemetryTrendingInterface
 
@@ -8,12 +9,19 @@ LRS_DATA_PATH = '/nfs/farm/g/glast/u42/ISOC-flight/FswDumps/reports/nonEvent/'
 DATA_BLOCK_SIZE = {'f1001': 19, 'f1002': 35, 'f1003': 28}
 OUTPUT_DIR_PATH = '/nfs/slac/g/glast/users/glground/lbaldini/saa/telemetry/'
 
-filesList = []
-for fileName in os.listdir(LRS_DATA_PATH):
-    for (pattern, blockSize) in DATA_BLOCK_SIZE.items():
-        if pattern in fileName:
-            filePath = os.path.join(LRS_DATA_PATH, fileName)
-            interface = telemetryTrendingInterface(filePath, OUTPUT_DIR_PATH,\
-                                                   blockSize)
-            print
-            print 
+filesList = commands.getoutput('ls %s*/f100*' % LRS_DATA_PATH).split('\n')
+numFilesLeft = len(filesList)
+print '%d files found.' % numFilesLeft
+
+for filePath in filesList:
+    numFilesLeft -= 1
+    if os.path.getsize(filePath) == 0:
+        print '%s is empty.' % filePath
+    else:
+        for (pattern, blockSize) in DATA_BLOCK_SIZE.items():
+            if pattern in filePath:
+                interface = telemetryTrendingInterface(filePath,\
+                                                       OUTPUT_DIR_PATH,\
+                                                       blockSize)
+    print '%d files left to look at.' % numFilesLeft
+
