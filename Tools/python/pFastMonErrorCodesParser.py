28ba1284331809bd83d634e4f671312998a5cc15
lbaldini <>
2008-07-12 00:51:21 +0000

    Updated.

--- Tools/python/pFastMonErrorCodesParser.py
+++ Tools/python/pFastMonErrorCodesParser.py
@@ -5,13 +5,19 @@ from pHtmlWriter import *
 import sys
 sys.path.append('../../FastMon/python')
 
-from pError import *
+from pError      import *
+from pErrorEvent import *
 
 
 MAIN_TEXT =\
 """
 The following table contains some explanations about the FastMon error
 codes.</br>
+
+The FastMon tuple contains an unsigned int (32 bits) variable called
+error_summary encoding all the errors (if any) in the event. The correspondence
+between the error codes and the relative summary bits is contained
+in the table as well.
 """
 
 class pFastMonErrorCodesParser(pHtmlWriter):
@@ -19,12 +25,17 @@ class pFastMonErrorCodesParser(pHtmlWriter):
     def writeTable(self, outputFilePath):
         self.openPage(outputFilePath, 'FastMon error codes')
         self.addParagraph(MAIN_TEXT)
-        self.openTable(['Error code', 'Description'])
+        self.openTable(['Error code', 'Description', 'Summary bit',\
+                            'Summary value'])
         errorCodes = ERROR_DETAIL_LABELS_DICT.keys()
         errorCodes.sort()
         for errorCode in errorCodes:
+            summaryBit = ERROR_BITS_DICT[errorCode]
+            summaryValue = 2**summaryBit
+            summaryLabel = '0x%x (decimal %d)' % (summaryValue, summaryValue)
             explanation = getExplanation(errorCode)
-            self.writeTableLine([errorCode, explanation])
+            self.writeTableLine([errorCode, explanation, summaryBit,\
+                                     summaryLabel])
         self.closePage()
 
 
