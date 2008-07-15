054c3577df6356d51355ad2522c6485c86e71492
bregeon <>
2008-07-15 01:17:27 +0000

    Add the quantity AcdHitChannel__pha__ for the ACD occupancy - GDQMQ-1

--- FastMon/python/pAEMcontributionIteratorBase.py
+++ FastMon/python/pAEMcontributionIteratorBase.py
@@ -85,7 +85,7 @@ class pAEMcontributionIteratorBase(LDF.AEMcontributionIterator):
     def acd_tile_hitmap__header__(self, cable, header):
 	self.TreeMaker.getVariable('acd_tile_hitmap')[0] = header.hitMap()
 
-    ## @brief Fill acd_cable_tile_count tree branch.
+    ## @brief Fill AcdHitChannel tree branch [12,18].
     ## @param self
     #  The class instance.
     ## @param cable
@@ -95,6 +95,6 @@ class pAEMcontributionIteratorBase(LDF.AEMcontributionIterator):
     ## @param pha
     #  The tile pulse height.
 
-    def acd_cable_tile_count__pha__(self, cable, channel, pha):
-	self.TreeMaker.getVariable('acd_cable_tile_count')[cable] += 1
+    def AcdHitChannel__pha__(self, cable, channel, pha):
+	self.TreeMaker.getVariable('AcdHitChannel')[cable][channel] += 1
 
