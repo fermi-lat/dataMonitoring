2f043ba9fbc071d17641afd19fd84856e596d4e4
lbaldini <>
2008-08-06 13:48:00 +0000

    Bug fix.

--- Common/python/pAlarmInspector.py
+++ Common/python/pAlarmInspector.py
@@ -18,8 +18,12 @@ BASE_OTHERS_XML_DIR  = '../../DigiReconCalMeritCfg/'
 class pAlarmInspector:
 
     def __init__(self):
-        pass
-
+        self.AlgorithmDict = {}
+        for fileName in os.listdir(os.curdir):
+            if 'alg__' in fileName:
+                self.AlgorithmDict[fileName[5:-3]] = 0
+        self.PlotsDict = {}
+                
     def inspect(self, application, productType):
         logger.info('Inspecting %s %s...' % (application, productType))
         plotsFilePath = self.getPlotsXmlFilePath(application, productType)
@@ -41,13 +45,14 @@ class pAlarmInspector:
                         break
                 for alarm in set.getElementsByTagName('alarm'):
                     algorithm = pXmlBaseElement(alarm).getAttribute('function')
+                    self.AlgorithmDict[algorithm] += 1
                     plotsDict[objectName].append(algorithm)
-        keys = plotsDict.keys()
-        keys.sort()
-        for key in keys:
-            print '%s: %s' % (key, plotsDict[key])
+        self.PlotsDict['%s %s' % (application, productType)] = plotsDict
 
     def match(self, objectName, alarmName):
+        if 'Time::' in alarmName:
+            prefix = alarmName.split('_')[0]
+            alarmName = alarmName.replace('%s_' % prefix, '')
         return objectName.split('[')[0] == alarmName.replace('_*', '') 
         
     def getXmlDoc(self, filePath):
@@ -73,9 +78,43 @@ class pAlarmInspector:
         fileName = '%s_%s_alarms.xml' % (application, productType)
         return os.path.join(BASE_ALARMS_XML_DIR, fileName)
 
+    def writeOutputFile(self, outputFilePath):
+        outputFile = file(outputFilePath, 'w')
+        algList = []
+        for (algorithm, numInstances) in self.AlgorithmDict.items():
+            if numInstances > 0:
+                algList.append(algorithm)
+        for (key, plotsDict) in self.PlotsDict.items():
+            outputFile.writelines('* %s\n\n' % key)
+            outputFile.writelines('||Plot name|')
+            for algorithm in algList:
+                outputFile.writelines('|%s|' % algorithm)
+            outputFile.writelines('|\n')
+            keys = plotsDict.keys()
+            keys.sort()
+            for plotName in keys:
+                algorithms = plotsDict[plotName]
+                outputFile.writelines('|%s|' % plotName)
+                tempDict = {}
+                for algorithm in algList:
+                    tempDict[algorithm] = 0
+                for algorithm in algorithms:
+                    tempDict[algorithm] += 1
+                for algorithm in algList:
+                    n = tempDict[algorithm]
+                    if n == 0:
+                        n = ' '
+                    outputFile.writelines('%s|' % n)
+                outputFile.writelines('\n')
+            outputFile.writelines('\n')
+        outputFile.close()
+
 
 if __name__ == '__main__':
     inspector = pAlarmInspector()
     for application in ['digi', 'recon', 'merit']:
         for productType in ['eor', 'trend']:
             inspector.inspect(application, productType)
+    print 'Alarms found:'
+    print inspector.AlgorithmDict
+    inspector.writeOutputFile('test.txt')
