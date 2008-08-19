1ea7a77859f9d6809ae60caef689e3fcec6bd4f5
lbaldini <>
2008-08-19 09:05:14 +0000

    Number of degrees of freedom and reduced chisuare added to the details dict.

--- Common/python/pGenericFitAlgorithm.py
+++ Common/python/pGenericFitAlgorithm.py
@@ -1,6 +1,7 @@
 from pSafeROOT           import ROOT
 from pAlarmBaseAlgorithm import pAlarmBaseAlgorithm
 
+import pUtils
 
 
 ## @brief Base "virtual" class for implementing alarms which involve fitting
@@ -26,4 +27,13 @@ class pGenericFitAlgorithm(pAlarmBaseAlgorithm):
         ([value], [error]) = self.getFitOutput(fitFunction, [fitParameter])
         error *= numSigma
         self.Output.setValue(value, error)
+        chiSquare = fitFunction.GetChisquare()
+        dof = fitFunction.GetNDF()
+        try:
+            reducedChiSquare = chiSquare/dof
+        except ZeroDivisionError:
+            reducedChiSquare = 0
+        self.Output.setDictValue('Reduced chi square',
+                                 pUtils.formatNumber(reducedChiSquare))
+        self.Output.setDictValue('Degrees of freedom', dof)
         fitFunction.Delete()
