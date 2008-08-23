6cbae461f85c7d970060f328f3bf1536c86c2e13
bregeon <>
2008-08-23 09:40:42 +0000

    Give number of seconds elapsed to the ErrorHandler - GDQMQ-251

--- FastMon/python/pDataProcessor.py
+++ FastMon/python/pDataProcessor.py
@@ -353,13 +353,21 @@ class pDataProcessor:
     def finalize(self):
         self.StopTime = time.time()
         elapsedTime   = self.StopTime - self.StartTime
-        averageRate   = self.NumEvents/elapsedTime
-        self.TreeMaker.close()
-        print
+        averageRate   = self.NumEvents/elapsedTime        
+
+	#For the ErrorHandler get the number of seconds elapsed, assuming counters are fine... 
+	tmin = self.TreeMaker.RootTree.GetMinimum("event_timestamp")
+	tmax = self.TreeMaker.RootTree.GetMaximum("event_timestamp")
+	delta_time = int(tmax-tmin)
+	#Now closing the TTree
+	self.TreeMaker.close()
+
         logger.info('Processing stopped on %s.' % time.asctime())
         logger.info('%d events processed in %.2f s (%.2f Hz).\n' %\
                     (self.NumEvents, elapsedTime, averageRate))
+	
         self.ErrorHandler.NumProcessedEvents = self.NumEvents
+        self.ErrorHandler.SecondsElapsed     = delta_time
         self.ErrorHandler.writeXmlOutput(self.OutputErrorFilePath)
 
 
@@ -380,7 +388,7 @@ class pDataProcessor:
         writer.openTag('pDataProcessorSummary')
         writer.indent()
         writer.writeTag('num_events', {}, self.NumEvents)
-        writer.writeTag('elapsed_tile', {}, elapsedTime)
+        writer.writeTag('elapsed_time', {}, elapsedTime)
         writer.writeTag('average_rate', {}, averageRate)
         #writer.writeTag('current_time', {}, time.asctime())
         writer.backup()
