87b1eb55f72f83199c9552352881c42d1f67fbd9
bregeon <>
2008-08-26 14:14:10 +0000

    Add function LookupErrorCode to Lookup the symbolic name of an error code in FastMon - GDQMQ-257

--- Common/python/pGlobals.py
+++ Common/python/pGlobals.py
@@ -29,3 +29,30 @@ ROOT_BASKET_SIZE          = 1000000
 PLUS_INFINITY = 1.e24
 MINUS_INFINITY = -1.e24
 NAN = float('nan')
+
+
+# Add here useful independant functions
+def LookupErrorCode(obj, code):
+  """@param Lookup the symbolic name of an error code
+
+  Unfortunately, the handle error methods pass the error code as an unsigned
+  int rather than an enumeration type.  If we had the enumeration type, then
+  this function would be implemented as returning the code.__name__ value.
+
+  Instead, we look up all symbols in the supplied object and see check whether
+  any of them have the same value as the supplied code.  If so, that symbol's
+  name is returned.
+  
+  Ric Claus wrote this one.
+
+  @param obj The object containing the error symbols of interest
+  @param code The error code to look up
+  @return The symbol string, if found, else the code value as a string  
+  """
+  for name in dir(obj):
+    if name.startswith("ERR_"):
+      value = eval("obj." + name)
+      if value == code:
+        return name
+  return "No name found for error code: " + str(code)
+
