fc24854c9f7dbf4caeef677f1c636aa71e24735d
bregeon <>
2008-08-26 14:08:12 +0000

    Update error handling using a new function provided by Ric to automatically log the error type. TEM_BUG are still tagged independantly - GDQMQ-257

--- FastMon/python/pERRcontributionIteratorBase.py
+++ FastMon/python/pERRcontributionIteratorBase.py
@@ -5,6 +5,7 @@ import pSafeLogger
 logger = pSafeLogger.getLogger('pERRcontributionIteratorBase')
 
 import LDF
+from pGlobals  import *
 
 
 ## @brief Class implementing the error contribution iterator.
@@ -28,8 +29,6 @@ class pERRcontributionIteratorBase(LDF.ERRcontributionIterator):
     #  The offset.
     ## @param errorHandler
     #  The @ref pErrorHandler object.
-
-    __gtcc  = [3, 6, 2, 7, 0, 5, 1, 4]
     
     def __init__(self, event, contribution, offset, errorHandler):
 
@@ -47,11 +46,10 @@ class pERRcontributionIteratorBase(LDF.ERRcontributionIterator):
                                           long(self.theError().tkr()),
                                           long(self.theError().cal()))
 
-
     ## @brief Handle error function overload
-    #  Build using the LDFdumper as an example
-    #  More inspiration from the original c++ code implementation
-    #  of the handleError function in "ERRcontributionIterator.cpp"
+    #  From Ric Claus
+    #  
+    #  The LookupErrorCode function is in Common/python/pGlobals
     #
     ##  return code is:
     #   - negative to indicate bail immediately
@@ -68,66 +66,14 @@ class pERRcontributionIteratorBase(LDF.ERRcontributionIterator):
     #    708 : 'ERR_NonzeroPadding'}  # Padding was nonzero
 
     def handleError(self, event, code, p1, p2):
-
-    	 if code == LDF.ERRcontributionIterator.ERR_PastEnd:
-    	     logger.debug("handleError:ERR_PastEnd\n" \
-    	 	          "\tIterated past the end of the contribution by %d words at stage %d" \
-    	 	          % (p1, p2))
-	     self.ErrorHandler.fill('ERR_CONTRIB_ERROR', ['PAST_END', self.TemId, p1, p2])
-
-    	 elif code == LDF.ERRcontributionIterator.ERR_OddCcWordCount:
-    	     logger.debug("handleError:ERR_OddCcWordCount\n" \
-    	 	          "\tCC word count was found to be odd - not legal : p1=%d, p2=%d\n" \
-    	 	          % (p1, p2))
-	     self.ErrorHandler.fill('ERR_CONTRIB_ERROR', ['ODD_CC_WORD_COUNT', self.TemId, p1, p2])
-
-    	 elif code == LDF.ERRcontributionIterator.ERR_HighWordCount:
-    	     logger.debug("handleError:ERR_HighWordCount\n" \
-    	 	          "\tWord count is higher than max allowed : p1=%d, p2=%d\n" \
-    	 	          % (p1, p2))
-	     self.ErrorHandler.fill('ERR_CONTRIB_ERROR', ['HIGH_WORD_COUNT', self.TemId, p1, p2])
-
-    	 elif code == LDF.ERRcontributionIterator.ERR_NoOpaqueData:
-    	     logger.debug("handleError:ERR_NoOpaqueData\n" \
-    	 	          "\tUnexpectedly found no opaque data for error : p1=%d, p2=%d\n" \
-    	 	          % (p1, p2))
-	     self.ErrorHandler.fill('ERR_CONTRIB_ERROR', ['NO_OPAQUE_DATA', self.TemId, p1, p2])
-
-    	 elif code == LDF.ERRcontributionIterator.ERR_MissingParams:
-    	     logger.debug("handleError:ERR_MissingParams\n" \
-    	 	          "\t(Some) error params were unexpectedly missing : p1=%d, p2=%d\n" \
-    	 	          % (p1, p2))
-	     self.ErrorHandler.fill('ERR_CONTRIB_ERROR', ['MISSING_PARAMS', self.TemId, p1, p2])
-
-    	 elif code == LDF.ERRcontributionIterator.ERR_TEMbug:
-	     logger.debug("handleError:ERR_TEMBug\n"\
-	     		  "\tFor TEM %d contribution:\n"\
-             		  "\tTEM firmware bug: Cable phasing error with all tags\n"\
-             		  "\tidentical (%s) but different from event number tag (%s) by one.\n"%\
-             		  (self.TemId, p1, p2)) 		   
-	     self.ErrorHandler.fill('TEM_BUG', ['Same Wrong Tags', self.TemId, p1, p2])
-
-    	 elif code == LDF.ERRcontributionIterator.ERR_TooMuchPadding:
-    	     logger.debug("handleError:ERR_TooMuchPadding\n" \
-    	 	          "\tMore than the expected amount of padding found : p1=%d, p2=%d\n" \
-    	 	          % (p1, p2))
-	     self.ErrorHandler.fill('ERR_CONTRIB_ERROR', ['TOO_MUCH_PADDING', self.TemId, p1, p2])
-
-    	 elif code == LDF.ERRcontributionIterator.ERR_NonzeroPadding:
-    	     logger.debug("handleError:ERR_NonzeroPadding\n" \
-    	 	          "\tPadding was nonzero : p1=%d, p2=%d\n" \
-    	 	          % (p1, p2))
-	     self.ErrorHandler.fill('ERR_CONTRIB_ERROR', ['NON_ZERO_PADDING', self.TemId, p1, p2])
-
-    	 else:
-    	     logger.debug("UNKNOWN_ERROR\n"\
-	              	  "\tFor TEM %d contribution:\n"\
-                      	  "\tUnrecognized error code %d = 0x%08x with "\
-                      	  "\targuments %d = 0x%08x, %d = 0x%08x\n" %\
-                      	  (self.TemId, code, code, p1, p1, p2, p2))
-             self.ErrorHandler.fill('ERR_CONTRIB_ERROR', ['UNKNOWN_ERROR_CODE', self.TemId, p1, p2])
-
-    	 return code
+      s = LDF.ERRcontributionIterator.handleError(self, event, code, p1,p2)
+      errName = LookupErrorCode(self, code)[4:]
+      # We want to tag the TEM BUG events specifically
+      if errName == 'TEMbug':
+          self.ErrorHandler.fill('TEM_BUG', ['All tags off by one', self.TemId, p1, p2])
+      else:
+          self.ErrorHandler.fill('ERR_CONTRIB_ERROR', [errName, self.TemId, p1, p2])
+      return s
 
     ## @brief Dispatch function for GCCC errors.
     
