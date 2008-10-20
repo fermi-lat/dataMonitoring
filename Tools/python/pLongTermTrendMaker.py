40e0c8af679df906fefed3e4aa1ed0da4f3a82d5
lbaldini <>
2008-10-20 10:33:02 +0000

    Initial import.

---
+++ Tools/python/pLongTermTrendMaker.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+BASE_PATH = '/afs/slac.stanford.edu/u/gl/glast/datacatalog/prod/datacat'
+BASE_COMMAND = 'find'
+DEFAULT_SITE = 'SLAC_XROOT /Data/Flight/Level1/LPA'
+DEFAULT_SORT = 'nRun'
+
+
+class pDataCatalogQuery:
+
+    def __init__(self, group, minTime, maxTime = None, minDuration = 1000,
+                 intent = 'nomSciOps'):
+        self.Command = BASE_PATH
+        self.Command += ' %s' % BASE_COMMAND
+        self.Command += ' --group %s' % group
+        self.Command += ' --site %s' % DEFAULT_SITE
+        self.Command += ' --sort %s' % DEFAULT_SORT
+        self.Command += ' --filter '
+        self.Command += "'"
+        self.Command += 'sIntent=="%s"' % intent
+        self.Command += ' && nMetStart>%s && nMetStart<%s' % (minTime, maxTime)
+        self.Command += ' && (nMetStop - nMetStart)>%s' % minDuration
+        self.Command += "'"
+
+    def dumpList(self, filePath = None):
+        cmd = self.Command
+        if filePath is not None:
+            cmd += ' >> %s' % filePath
+        print 'Executing "%s"...' % cmd
+        os.system(cmd)
+        print 'Done.'
+
+
+if __name__ == '__main__':
+    query = pDataCatalogQuery('RECONHISTALARMDIST', 246100000, 246164127)
+    query.dumpList('test.txt')
