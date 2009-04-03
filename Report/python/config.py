6c86b0c127f2ee4a8ca49be7a5fdd6c21e1ece87
monzani <>
2009-04-03 20:48:26 +0000

    queue long is used by default

--- Report/python/config.py
+++ Report/python/config.py
@@ -8,7 +8,7 @@ import os
 import sys
 
 taskName = "launchReport"
-taskVersion = "1.4"
+taskVersion = "1.5"
 ReportMainDir = '/afs/slac.stanford.edu/g/glast/ground/releases/volume03/Report'
 
 doCleanup = True
