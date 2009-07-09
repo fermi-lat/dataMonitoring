bdf3f0b60b732ba6ec2fba14c369cc9a8a6a791a
lbaldini <>
2009-07-09 09:08:10 +0000

    Test program added.

---
+++ FastMon/python/testSAA.py
@@ -0,0 +1,57 @@
+#!/bin/env python
+
+from pM7Parser   import *
+from pSafeROOT   import *
+from pSAAPolygon import *
+
+
+SAA_XML_FILE_PATH =\
+   '/data/work/datamon/dataMonitoring/FastMonCfg/xml/saaDefinition.xml'
+
+EARTH_GRID.GetYaxis().SetRangeUser(-30, 30)
+
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    (opts, arguments) = parser.parse_args()
+    if len(arguments) != 1:
+        parser.error('Please provide a (single) m7 file.')
+        sys.exit()
+    inputFilePath = arguments[0]
+    gLon = ROOT.TGraph()
+    gLat = ROOT.TGraph()
+    gOrb = ROOT.TGraph()
+    gSAADist = ROOT.TGraph()
+    gSAAFlag = ROOT.TGraph()
+    for g in [gLon, gLat, gSAADist, gSAAFlag, gOrb]:
+        g.SetMarkerStyle(20)
+        g.SetMarkerSize(0.4)
+    gSAAFlag.SetLineColor(ROOT.kRed)
+    gSAAFlag.SetMarkerColor(ROOT.kRed)
+    parser = pM7Parser(inputFilePath, SAA_XML_FILE_PATH)
+    parser.parseIt()
+    for (i, met) in enumerate(parser.TimePoints):
+        pos = parser.getSCPosition((met,0))
+        pos.processCoordinates()
+        lon = pos.getLongitude()
+        lat = pos.getLatitude()
+        dsaa = pos.getDistanceToSAA()
+        fsaa = float(pos.OrbInSAA)
+        if (dsaa < 0 and fsaa < 0) or (dsaa > 0 and fsaa > 0): 
+            print met, lon, lat, dsaa, fsaa
+        gLon.SetPoint(i, met, lon)
+        gLat.SetPoint(i, met, lat)       
+        gOrb.SetPoint(i, lon, lat)
+        gSAADist.SetPoint(i, met, dsaa)
+        gSAAFlag.SetPoint(i, met, 1000*fsaa)
+
+    canvas = ROOT.TCanvas('saa', inputFilePath)
+    canvas.Divide(2, 2)
+    canvas.cd(1); gLon.Draw('ap')
+    canvas.cd(2); gLat.Draw('ap')
+    canvas.cd(3); gSAADist.Draw('ap'); gSAAFlag.Draw('p,same')
+    canvas.cd(4); EARTH_GRID.Draw();
+    gOrb.Draw('p,same'); parser.SAAPolygon.draw('same')
+    canvas.Update()
+    raw_input('Press enter to quit.')
