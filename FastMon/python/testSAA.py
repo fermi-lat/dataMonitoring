24ad59236c887d86b20b42c8e923b5d23432ac6d
lbaldini <>
2009-07-09 09:46:00 +0000

    Some improvements in the output plots.

--- FastMon/python/testSAA.py
+++ FastMon/python/testSAA.py
@@ -4,10 +4,10 @@ from pM7Parser   import *
 from pSafeROOT   import *
 from pSAAPolygon import *
 
+ROOT.gStyle.SetOptStat(0)
 
 SAA_XML_FILE_PATH =\
    '/data/work/datamon/dataMonitoring/FastMonCfg/xml/saaDefinition.xml'
-
 EARTH_GRID.GetYaxis().SetRangeUser(-30, 30)
 
 
@@ -45,13 +45,23 @@ if __name__ == '__main__':
         gOrb.SetPoint(i, lon, lat)
         gSAADist.SetPoint(i, met, dsaa)
         gSAAFlag.SetPoint(i, met, 1000*fsaa)
-
+    for g in [gLon, gLat, gSAADist, gSAAFlag]:
+        g.GetXaxis().SetTitle('Mission elapsed Time (s)')
     canvas = ROOT.TCanvas('saa', inputFilePath)
     canvas.Divide(2, 2)
-    canvas.cd(1); gLon.Draw('ap')
-    canvas.cd(2); gLat.Draw('ap')
-    canvas.cd(3); gSAADist.Draw('ap'); gSAAFlag.Draw('p,same')
-    canvas.cd(4); EARTH_GRID.Draw();
-    gOrb.Draw('p,same'); parser.SAAPolygon.draw('same')
+    canvas.cd(1)
+    gLon.GetYaxis().SetTitle('Longitude (degrees)')
+    gLon.Draw('ap')
+    canvas.cd(2)
+    gLat.GetYaxis().SetTitle('Latitude (degrees)')
+    gLat.Draw('ap')
+    canvas.cd(3)
+    gSAADist.GetYaxis().SetTitle('Distance to SAA (km)')
+    gSAADist.Draw('ap')
+    gSAAFlag.Draw('p,same')
+    canvas.cd(4)
+    EARTH_GRID.Draw();
+    gOrb.Draw('p,same')
+    parser.SAAPolygon.draw('same')
     canvas.Update()
     raw_input('Press enter to quit.')
