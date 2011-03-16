2e4c05070b14741bc05e6af2187c734f9a74b99b
lbaldini <>
2011-03-16 12:03:23 +0000

    Semi-working version.

--- Tools/python/pSolarFlareTrendMerger.py
+++ Tools/python/pSolarFlareTrendMerger.py
@@ -14,9 +14,9 @@ DEFAULT_SITE = 'SLAC_XROOT /Data/Flight/Level1/LPA'
 DEFAULT_SORT = 'nRun'
 
 VARIABLE_DICT = {
-    'Digi_Trend_Bin_Start'                              : ('l', ''     ),
-    'Digi_Trend_TimeStampFirstEvt'                      : ('D', ''     ),
-    'Digi_Trend_TimeStampLastEvt'                       : ('D', ''     ),
+    'Digi_Trend_Bin_Start'                              : ('I', ''     ),
+    'Digi_Trend_Bin_End'                                : ('I', ''     ),
+    'Digi_Trend_TrueTimeInterval'                       : ('D', ''     ),
     'Digi_Trend_Rate_AcdDigis'                          : ('F', ''     ),
     'Digi_Trend_Rate_FswFilters_GAMMA'                  : ('F', ''     ),
     'Digi_Trend_OutF_Normalized_AcdHit_AcdTile'         : ('F', '[128]'),
@@ -24,7 +24,10 @@ VARIABLE_DICT = {
     'FastMon_Trend_Mean_FastMon_SpaceCraft_RockAngle'   : ('F', ''     ),
     'Merit_Trend_OutF_NormRateTransientEvts'            : ('F', ''     ),
     'Merit_Trend_Rate_TransientEvts'                    : ('F', ''     ),
-    'Merit_Trend_Rate_TransientEvtsBelowZenithTheta100' : ('F', ''     )
+    'Merit_Trend_Rate_TransientEvtsBelowZenithTheta100' : ('F', ''     ),
+    'Merit_Trend_Mean_PtLat'                            : ('F', ''     ),
+    'Merit_Trend_Mean_PtLon'                            : ('F', ''     ),
+    'Merit_Trend_Mean_PtMcIlwainL'                      : ('F', ''     )
     }
 
 GROUP_LIST  = ['DIGITREND', 'FASTMONTREND', 'MERITTREND']
@@ -72,9 +75,9 @@ class pDataCatalogQuery:
 
 class pSolarFlareTrendMerger:
 
-    def __init__(self, outputFilePath, recreateFileLists = True,
+    def __init__(self, fileListPath, outputFilePath,
                  minStartDate = None, maxStartDate = None):
-        if recreateFileLists:
+        if not os.path.exists(fileListPath):
             print 'Creating the file lists...'
             if minStartDate is not None:
                 minStartTime = utc2met(convert2sec(minStartDate))
@@ -84,39 +87,18 @@ class pSolarFlareTrendMerger:
                 maxStartTime = utc2met(convert2sec(maxStartDate))
             else:
                 maxStartTime = None
-            minRunDuration = 10
-            runIntents = ['nomSciOps', 'nomSciOps_diagEna']
-            for group in self.GROUPS:
-                query = pDataCatalogQuery(group, minStartTime, maxStartTime,
-                                          minRunDuration, runIntents)
-                query.dumpList(self.getFileListPath(group))
-                logFilePath = outputFilePath.replace('.root', '_group.log')
-                logFile = file(logFilePath, 'w')
-                logFile.writelines('Created by pSolarFlareTrendMerger on %s.' %\
-                                       time.asctime())
-                logFile.writelines('\n\n')
-                logFile.writelines('Selections for histogram merging:\n')
-                logFile.writelines('- Start run between %s and %s (UTC).\n' %\
-                                       (minStartDate, maxStartDate))
-                logFile.writelines('- Minimum run duration: %s s.\n' %\
-                                       minRunDuration)
-                logFile.writelines('- Run intent: "%s".\n' % runIntents)
-                logFile.writelines('\n')
-                logFile.writelines('See the file lists for details. Bye.')
-                logFile.close()
-
-        self.FileListDict = {}
-        for group in self.GROUPS:
-            filePath = self.getFileListPath(group)
-            self.FileList[group] =\
-                [line.strip('\n') for line in file(filePath, 'r')]
-            self.FileList[group].sort()
+            query = pDataCatalogQuery(minStartTime, maxStartTime)
+            query.dumpList(fileListPath)
+        else:
+            print 'File list %s found.' % fileListPath
+            print 'Delete the file if you want to recreate it.'
+        self.FileList = [line.strip('\n') for line in file(fileListPath, 'r')]
         self.OutputFilePath = outputFilePath
         self.VariableDict = VARIABLE_DICT
+        self.DigiTree = None
+        self.FastmonTree = None
+        self.MeritTree = None
         print 'Done. %d file(s) found.' % len(self.FileList)
-
-    def getFileListPath(self, group):
-        return 'SOLARFLARE_%s_FILELIST.txt' % group
 
     def createArrays(self):
         print 'Creating arrays...'
@@ -138,37 +120,52 @@ class pSolarFlareTrendMerger:
 
     def copyArrays(self):
         for (name, (type, shape)) in self.VariableDict.items():
+            if name.startswith('Digi_Trend_'):
+                nn = name.replace('Digi_Trend_', '')
+                self.DigiTree.SetBranchAddress(nn, self.InputArrayDict[name])
+            elif name.startswith('FastMon_Trend_'):
+                nn = name.replace('FastMon_Trend_', '')
+                self.FastmonTree.SetBranchAddress(nn, self.InputArrayDict[name])
+            elif name.startswith('Merit_Trend_'):
+                nn = name.replace('Merit_Trend_', '')
+                self.MeritTree.SetBranchAddress(nn, self.InputArrayDict[name])
             if shape == '':
                 self.OutputArrayDict[name][0] = self.InputArrayDict[name][0]
-            elif shape == '[16]':
-                for i in range(16):
+            elif shape == '[128]':
+                for i in range(128):
                     self.OutputArrayDict[name][i] =\
                          self.InputArrayDict[name][i]
-            elif shape == '[16][36]':
-                for i in range(16):
-                    for j in range(36):
-                        self.OutputArrayDict[name][i][j] =\
-                             self.InputArrayDict[name][i][j]
 
     def run(self, local = False):
         self.OutputFile = ROOT.TFile(self.OutputFilePath, 'RECREATE')
         self.OutputTree = ROOT.TTree('Time', 'Time')
         self.createArrays()
-        for filePath in self.FileList:
-            print 'Looping over %s...' % filePath
+        numRuns = len(self.FileList)/3
+        for i in xrange(numRuns):
+            (digiPath, fastmonPath, meritPath) = self.FileList[i*3:i*3+3]
+            print 'Looping over run %d/%d' % (i + 1, numRuns)
+            print '\tDigi   : %s' % digiPath
+            print '\tFastmon: %s' % fastmonPath
+            print '\tMerit  : %s' % meritPath
             if local:
-                rootFile = ROOT.TFile(filePath)
+                digiFile    = ROOT.TFile(digiPath)
+                fastmonFile = ROOT.TFile(fastmonPath)
+                meritFile   = ROOT.TFile(meritPath)
             else:
-                rootFile = ROOT.TXNetFile(filePath)
-            rootTree = rootFile.Get('Time')
-            for name in self.VariableDict.keys():
-                rootTree.SetBranchAddress(name, self.InputArrayDict[name])
-            numEntries = rootTree.GetEntries()
+                digiFile    = ROOT.TXNetFile(digiPath)
+                fastmonFile = ROOT.TXNetFile(fastmonPath)
+                meritFile   = ROOT.TXNetFile(meritPath)
+            self.DigiTree = digiFile.Get('Time')
+            self.FastmonTree = fastmonFile.Get('Time')
+            self.MeritTree = meritFile.Get('Time')
+            # Wanna check that the three trees have the same number of entries?
+            numEntries = self.DigiTree.GetEntries()
             for i in xrange(numEntries):
-                rootTree.GetEntry(i)
+                self.DigiTree.GetEntry(i)
+                self.FastmonTree.GetEntry(i)
+                self.MeritTree.GetEntry(i)
                 self.copyArrays()
                 self.OutputTree.Fill()
-            rootFile.Close()
         self.OutputFile.cd()
         self.OutputTree.Write()
         self.OutputFile.Close()
@@ -176,10 +173,9 @@ class pSolarFlareTrendMerger:
 
         
 if __name__ == '__main__':
-    q = pDataCatalogQuery()
-    q.dumpList('testlist.txt')
-    #dumpVarDict('r0288475156_tkrtrend.root')
-    #merger = pTkrTrendMerger('tkrtrend_filelist.txt', 'tkrtrend.root',
-    #None, None)
-    #                         'Jan/15/2010 00:00:00', 'Jan/20/2010 00:00:00')
-    #merger.run(False)
+    #q = pDataCatalogQuery()
+    #q.dumpList('testlist.txt')
+    merger = pSolarFlareTrendMerger('solarflare_filelist.txt',
+                                    'solarflare_trend.root',
+                                    'Jan/01/2010 00:00:00')
+    merger.run(False)
