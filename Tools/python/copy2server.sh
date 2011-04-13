01d6adc7d39b73c25f5952384d022efff5401853
monzani <>
2011-04-13 06:51:37 +0000

    correct location

--- Tools/python/copy2server.sh
+++ Tools/python/copy2server.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-scp $1 noric01.slac.stanford.edu:/nfs/slac/g/glast/ground/links/Monitoring/Documentation/Miscellanea
+scp $1 noric01.slac.stanford.edu:/nfs/slac/g/glast/ground/links/Documentation/Miscellanea
